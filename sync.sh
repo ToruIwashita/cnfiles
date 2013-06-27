b658e632dedc706fe2a5b8c6bbc09c612f27300c
Toru Iwashita <toru.iwashita@gmail.com>
2013-06-27 13:55:11 +0900

    - vim関連設定ファイル構成変更

--- sync.sh
+++ sync.sh
@@ -8,11 +8,15 @@
 [ -d ~/.zsh.d/zfunc_local ] || mkdir ~/.zsh.d/zfunc_local
 [ -d ~/.zsh.d/_zfunc_local ] || mkdir ~/.zsh.d/_zfunc_local
 [ -d ~/.vim ] || mkdir ~/.vim
+[ -f ~/.vim/vconf ] || touch ~/.vim/vconf
+[ -f ~/.vim/abbreviate_conf ] || touch ~/.vim/abbreviate_conf
 [ -d ~/.vim/plugin ] || mkdir ~/.vim/plugin
 
 \cp -p ./zshrc ~/.zshrc
 \cp -p ./tmux.conf ~/.tmux.conf
 \cp -p ./vimrc ~/.vimrc
+\cp -p ./vim/vconf ~/.vim/vconf
+\cp -p ./vim/abbreviate_conf ~/.vim/abbreviate_conf
 \cp -p ./zsh.d/zconf ~/.zsh.d/zconf
 \cp -p ./zsh.d/ls_colors ~/.zsh.d/ls_colors
 
