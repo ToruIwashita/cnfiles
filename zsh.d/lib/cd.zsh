a039cfd901402ff3c855b46f7944afe68da95330
ToruIwashita <toru.iwashita@gmail.com>
2014-03-19 00:07:42 +0900

    - zsh.d/lib/cd.zsh,cdup時に明示的にprecmdの関数を呼び出す

--- zsh.d/lib/cd.zsh
+++ zsh.d/lib/cd.zsh
@@ -1,6 +1,12 @@
 ## cd関連関数
 functions cdup() {
+  local precmd_func
+
   cd ..
+  type precmd > /dev/null 2>&1 && precmd 
+  for precmd_func in $precmd_functions; do
+    $precmd_func
+  done
   zle reset-prompt
 }
 
