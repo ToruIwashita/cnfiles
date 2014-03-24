d3fb20b9cbddcfa48a2515499b7e09b6da5d2dea
ToruIwashita <toru.iwashita@gmail.com>
2014-03-24 23:23:55 +0900

    - zsh.d/functions/double_zccept_line.zsh,関数名,ファイル名を修正

--- zsh.d/functions/double_zccept_line.zsh
+++ zsh.d/functions/double_zccept_line.zsh
@@ -1,7 +1,7 @@
 ## double-accept-line
-functions double-accept-line() {
+functions double_accept_line() {
   zle accept-line
   zle accept-line
 }
 
-zle -N double-accept-line
+zle -N double_accept_line
