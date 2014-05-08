9d9934f0a3a45635296b5f5037659a4323427b44
ToruIwashita <toru.iwashita@gmail.com>
2014-05-08 23:52:38 +0900

    - into_dir_and_push_tail_to_prompt.zsh更新 - Ctr+Oにinto-dir-and-push-tail-to-promptを設定

--- zsh.d/functions/into_dir_and_push_tail_to_prompt.zsh
+++ zsh.d/functions/into_dir_and_push_tail_to_prompt.zsh
@@ -1,20 +1,35 @@
 ## into-dir-and-push-tail-to-prompt
 functions _into-dir-and-push-tail-to-prompt() {
-  local args chpwd_func file_path
+  local chpwd_func arg_index file_path dir_path
+  local -a args
   args=($(print $BUFFER))
   chpwd_func=$(typeset -f chpwd)
 
   exec < /dev/tty
+
   if [[ $#args -ge 2 ]]; then
-    file_path=$args[2]
+    arg_index=2
   else
-    file_path=$args[1]
+    arg_index=1
+  fi
+  file_path=$args[$arg_index]
+
+  if [[ $file_path:h = '.' ]]; then
+    args[$arg_index]='./'
+    dir_path=$file_path:t
+  else
+    args[$arg_index]=$file_path:t
+    dir_path=$file_path:h
   fi
 
   chpwd() { }
-  cd $file_path:h
-  zle reset-prompt
+  cd $dir_path
   eval $chpwd_func
+
+  zle reset-prompt
+  zle kill-whole-line
+  BUFFER=$args
+  zle end-of-line
 }
 
 zle -N into-dir-and-push-tail-to-prompt _into-dir-and-push-tail-to-prompt
