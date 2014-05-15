f06190e674ade6882e23163d5fc26c0b59a46f44
ToruIwashita <toru.iwashita@gmail.com>
2014-05-15 23:36:16 +0900

    - zsh,agのラッパー関数とラッパー補完関数削除

--- zsh.d/local/completions/_ag.zsh
+++
@@ -1,7 +0,0 @@
-# ag用補完関数
-_ag() {
-  _arguments : \
-    '1: :_files'
-}
-
-compdef _ag ag
--- zsh.d/local/functions/ag.zsh
+++
@@ -1,9 +0,0 @@
-## ag関数(引数順序替え)
-ag() {
-  if [[ $# -lt 1 ]]; then
-    print "lack of arguments."
-    return 1
-  fi  
-
-  eval "$AG_BIN $2 $1"
-}
