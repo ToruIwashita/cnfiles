602cc5c981382758582307d9d27913472cdbad96
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-06-24 10:37:23 +0900

    - zsh,cat-current修正

--- zsh.d/widgets/cat_current.zsh
+++ zsh.d/widgets/cat_current.zsh
@@ -2,8 +2,10 @@
 _cat-current() {
   local -a args
   args=("${(z)BUFFER}")
-  zle -I
-  cat ${args[$#args]}
+  if [[ -n ${args[$#args]} ]]; then
+    zle -I
+    cat ${args[$#args]}
+  fi
 }
 
 zle -N cat-current _cat-current
