6aaea3d65bb7b66d191ae731c97b3d60ada196a5
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-07-16 17:58:27 +0900

    - zsh,peco_silver_search_and_start_editor.zshの不要な変数除去

--- zsh.d/widgets/peco_silver_search_and_start_editor.zsh
+++ zsh.d/widgets/peco_silver_search_and_start_editor.zsh
@@ -1,6 +1,6 @@
 ## peco_silver_search_and_start_editor
 _peco-silver-search-and-start-editor() {
-  local arg ag_output resource_info resource_path line_number cmd
+  local arg resource_info resource_path line_number cmd
 
   resource_info=$(ag "$BUFFER" 2>/dev/null | peco 2>/dev/null)
   [[ -z $resource_info ]] && return
