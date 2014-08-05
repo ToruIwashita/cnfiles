a36c517db2dfc7486ebceb3dea8281b9794b46fd
ToruIwashita <toru.iwashita@gmail.com>
2014-08-05 23:38:55 +0900

    - zsh,chpwd_recent_filehandlerを扱うwidgetsでchpwd-recent-dirsのsuffixに/を加えるように変更

--- zsh.d/widgets/cdup.zsh
+++ zsh.d/widgets/cdup.zsh
@@ -2,7 +2,7 @@
 _cdup() {
   local precmd_func after_buffer
   local -a args
-  local -aU reply
+  local -aU reply reply_with_slash_suffix
   args=("${(z)BUFFER}")
 
   if [[ $#args -ge 2 ]]; then
@@ -16,6 +16,9 @@ _cdup() {
   if (( ${+functions[chpwd_recent_filehandler]} && ${+functions[chpwd_recent_add]} )); then
     chpwd_recent_filehandler
     chpwd_recent_add $PWD && chpwd_recent_filehandler $reply
+    chpwd_recent_filehandler
+    reply_with_slash_suffix=(${^reply%/}/)
+    chpwd_recent_filehandler $reply_with_slash_suffix
   fi
 
   (( ${+functions[precmd]} )) && precmd
