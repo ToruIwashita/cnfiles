22824a2eaa8a44a4609d291f9a5768a4637eff82
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-08-06 15:15:07 +0900

    - _menu_complete_recent_dirs.zsh更新

--- zsh.d/completion_widgets/_menu_complete_recent_dirs.zsh
+++ zsh.d/completion_widgets/_menu_complete_recent_dirs.zsh
@@ -1,3 +1,14 @@
 # _menu_complete_recent_dirs
+__complete-recent-dirs() {
+  local -a recent_dirs
+  if (( ${+functions[cdr]} )); then
+    recent_dirs=(${^${(f)"$(cdr -l)"}##*[[:space:]]}/)
+  else
+    recent_dirs=()
+  fi
+  echo $recent_dirs
+  compadd -Q -a recent_dirs
+}
+
 zle -C menu-complete-recent-dirs menu-complete _generic
-zstyle ':completion:menu-complete-recent-dirs:*' completer _cdr
+zstyle ':completion:menu-complete-recent-dirs:*' completer __complete-recent-dirs
