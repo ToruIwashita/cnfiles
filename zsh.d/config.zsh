56fefb82d58f266c70486bde568803411dec6d53
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-08-06 15:03:58 +0900

    Revert "- config.zsh,_chpwd_recent_add_slash_suffix()を追加"
    This reverts commit d4e2ab5f574015a44e7a0fb8a89828d72dcc2d88.

--- zsh.d/config.zsh
+++ zsh.d/config.zsh
@@ -149,21 +149,12 @@ _precmd_vcs_info() {
 _chpwd_ls() {
   [[ $dirstack[1]:h != $PWD ]] && ls
 }
-# _chpwd_recent_add_slash_suffix
-_chpwd_recent_add_slash_suffix() {
-  if (( ${+functions[chpwd_recent_filehandler]} && ${+functions[chpwd_recent_add]} )); then
-    local -a reply_with_slash_suffix
-    chpwd_recent_filehandler
-    reply_with_slash_suffix=(${^reply%/}/)
-    chpwd_recent_filehandler $reply_with_slash_suffix
-  fi
-}
 
 ## auto-execution of functions
 # precmd_functions設定
 precmd_functions+='_precmd_vcs_info'
 # chpwd_functions設定
-chpwd_functions=(chpwd_recent_dirs _chpwd_ls _chpwd_recent_add_slash_suffix)
+chpwd_functions=(chpwd_recent_dirs _chpwd_ls)
 
 ## widgets
 # PROMPT_VIM_MODE_COLOR選択widgets
