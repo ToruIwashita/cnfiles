43af9468c63458ad0346dbf25fa252a79dd93d7e
ToruIwashita <toru.iwashita@gmail.com>
2014-08-07 23:43:33 +0900

    - peco_silver_search_and_start_editor.zsh複数ファイル対応

--- zsh.d/widgets/peco_silver_search_and_start_editor.zsh
+++ zsh.d/widgets/peco_silver_search_and_start_editor.zsh
@@ -1,21 +1,39 @@
 ## peco_silver_search_and_start_editor
 _peco-silver-search-and-start-editor() {
-  integer line_number
-  local resource_info file_path
+  local _resource_info resource_info specified_line
+  local -a peco_resulting_line
+  local -aU file_path
 
-  resource_info=$(ag "$BUFFER" 2>/dev/null | peco 2>/dev/null)
-  if [[ -z $resource_info ]]; then
+  peco_resulting_line=(${(f)"$(ag "$BUFFER" 2>/dev/null | peco 2>/dev/null)"})
+  if [[ -z $peco_resulting_line ]]; then
     zle beginning-of-line
     return 0
   fi
 
-  line_number=${${resource_info#*:}%%:*}
-  file_path=${resource_info%%:*}
+  if [[ $#peco_resulting_line -eq 1 ]]; then
+    specified_line=+${${peco_resulting_line#*:}%%:*}
+    resource_info=${peco_resulting_line%%:*}
+    if [[ $resource_info =~ '^~' ]]; then
+      file_path+=($HOME${resource_info#*~})
+    else
+      file_path+=($resource_info)
+    fi
+  else
+    specified_line=''
+    resource_info=${(R)peco_resulting_line%%:*}
+    for _resource_info in $resource_info; do
+      if [[ $_resource_info =~ '^~' ]]; then
+        file_path+=($HOME${_resource_info#*~})
+      else
+        file_path+=($_resource_info)
+      fi
+    done
+  fi
 
   zle -I
-  print -s $EDITOR:t $file_path +$line_number
-  [[ $file_path =~ '^~' ]] && file_path=$HOME${file_path#*~}
-  $EDITOR $file_path +$line_number < $TTY
+  print -s $EDITOR:t $resource_info $specified_line
+  echo $specified_line
+  $EDITOR $file_path $specified_line < $TTY
 }
 
 zle -N peco-silver-search-and-start-editor _peco-silver-search-and-start-editor
