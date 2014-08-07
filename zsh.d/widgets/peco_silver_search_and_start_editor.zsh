424d4a7389111f448a988abc44aaa76024184aa5
ToruIwashita <toru.iwashita@gmail.com>
2014-08-08 00:15:51 +0900

    - peco_silver_search_and_start_editor.zshの不要なコードを整理

--- zsh.d/widgets/peco_silver_search_and_start_editor.zsh
+++ zsh.d/widgets/peco_silver_search_and_start_editor.zsh
@@ -1,8 +1,8 @@
 ## peco_silver_search_and_start_editor
 _peco-silver-search-and-start-editor() {
-  local _resource_info specified_line
+  local specified_line
   local -a peco_resulting_line
-  local -aU file_path resource_info
+  local -aU file_path
 
   peco_resulting_line=(${(f)"$(ag "$BUFFER" 2>/dev/null | peco 2>/dev/null)"})
   if [[ -z $peco_resulting_line ]]; then
@@ -12,18 +12,14 @@ _peco-silver-search-and-start-editor() {
 
   if [[ $#peco_resulting_line -eq 1 ]]; then
     specified_line=+${${peco_resulting_line#*:}%%:*}
-    resource_info=${peco_resulting_line%%:*}
-    file_path=$resource_info
+    file_path=${peco_resulting_line%%:*}
   else
     specified_line=''
-    resource_info=(${(R)peco_resulting_line%%:*})
-    for _resource_info in $resource_info; do
-      file_path+=($_resource_info)
-    done
+    file_path=(${(R)peco_resulting_line%%:*})
   fi
 
   zle -I
-  print -s $EDITOR:t $resource_info $specified_line
+  print -s $EDITOR:t $file_path $specified_line
   $EDITOR $file_path $specified_line < $TTY
 }
 
