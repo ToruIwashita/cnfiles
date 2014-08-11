838ef04cd7fd662f7907d3b025c6a33ba5586bdc
ToruIwashita <toru.iwashita@gmail.com>
2014-08-11 22:35:06 +0900

    - peco_cd_recent_dir.zsh追加

---
+++ zsh.d/widgets/peco_cd_recent_dir.zsh
@@ -0,0 +1,17 @@
+## peco-cd-recent-dir
+_peco-cd-recent-dir() {
+  local -a recent_dirs
+  local -aU dir_path
+
+  if (( !${+functions[cdr]} )); then
+    return 0
+  fi
+
+  recent_dirs=${(F)${${(f)"$(cdr -l)"}##<-> ##}}
+  dir_path=(${(f)"$(echo $recent_dirs | peco 2>/dev/null)"})
+
+  zle kill-whole-line
+  BUFFER=$dir_path
+  zle accept-line
+}
+zle -N peco-cd-recent-dir _peco-cd-recent-dir
