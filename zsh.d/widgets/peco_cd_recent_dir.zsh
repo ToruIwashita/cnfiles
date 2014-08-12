4a34a3e555928dd24b05500863c221e04f0531b6
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-08-12 12:47:48 +0900

    - peco_cd_recent_dir.zsh,pecoの結果が0の場合return

--- zsh.d/widgets/peco_cd_recent_dir.zsh
+++ zsh.d/widgets/peco_cd_recent_dir.zsh
@@ -9,6 +9,10 @@ _peco-cd-recent-dir() {
 
   recent_dirs=${(F)${${(f)"$(cdr -l)"}##<-> ##}}
   dir_path=(${(f)"$(echo $recent_dirs | peco 2>/dev/null)"})
+  if [[ -z $dir_path ]]; then
+    zle beginning-of-line
+    return 0
+  fi
 
   zle kill-whole-line
   BUFFER="cd $dir_path"
