867d804a6127ff247d4c2c60da325d8499f3f6e6
ToruIwashita <toru.iwashita@gmail.com>
2014-08-14 23:16:19 +0900

    - peco_kill_ps.zsh修正

--- zsh.d/functions/peco_kill_ps.zsh
+++ zsh.d/functions/peco_kill_ps.zsh
@@ -1,8 +1,21 @@
 ## peco-kill-ps
 peco-kill-ps() {
   integer pid
+  local answer
+
   pid=$(ps -ef | peco | awk '{ print $2 }')
-  if [[ -n $pid ]]; then
-    kill $pid
+
+  if [[ $pid -eq 0 ]]; then
+    return 0
   fi
+
+  while :; do
+    print -n "Kill process $pid (y/n)? "
+    read answer
+    case "$answer" in
+      [yY]) kill $pid; return 0 ;;
+      [nN]) return 0 ;;
+      *) print -n 'Please enter y or n. ' ;;
+    esac
+  done
 }
