0ba5da0e8505c23dec2ebbb1ae5bac6f1d8cf0a4
Toru Iwashita <toru.iwashita@gmail.com>
2014-08-15 16:45:04 +0900

    - peco_kill_ps.zsh,複数プロセスを選択可能に変更

--- zsh.d/functions/peco_kill_ps.zsh
+++ zsh.d/functions/peco_kill_ps.zsh
@@ -1,25 +1,29 @@
 ## peco-kill-ps
 peco-kill-ps() {
-  integer pid
-  local answer
+  local pid answer
+  local -a pids
 
-  pid=$(ps -ef | peco | awk '{ print $2 }')
+  pids=(${(f)"$(ps -ef | peco | awk '{ print $2 }')"})
 
-  if [[ $pid -eq 0 ]]; then
+  if [[ -z $pids ]]; then
     return 0
   fi
 
-  while :; do
-    print -n "Kill process $pid (y/n)? "
-    read answer
-    case "$answer" in
-      [yY]) kill $pid
-            break
-            ;;
-      [nN]) break
-            ;;
-      *)    print -n 'Please enter y or n. '
-            ;;
-    esac
+  for pid in $pids; do
+    while :; do
+      print -n "Kill process $pid (y/n)? "
+      read answer
+      case "$answer" in
+        [yY]) kill $pid
+              break
+              ;;
+        [nN]) break
+              ;;
+        *)    print -n 'Please enter y or n. '
+              ;;
+      esac
+    done
   done
+
+  return 0
 }
