b1754cc370e9149c19c07167e0c99bf2e8d10d6c
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-08-18 18:08:27 +0900

    - config.local.zshにgoの設定を追加

--- zsh.local.d/config.local.zsh
+++ zsh.local.d/config.local.zsh
@@ -1,35 +1,31 @@
 ## 環境変数設定
 readonly MYSQL_CNCT="mysql test"
 readonly MYSQL_CMD="$MYSQL_CNCT -e"
-readonly AG_BIN=~/local/bin/ag
 
-export EDITOR=~/local/bin/vim                          # ~/local/bin/vimをEDITOR設定
+export EDITOR=~/local/bin/vim
+export GOROOT=~/local/go
+export GOPATH=~/.go
 export MYSQL_CNCT MYSQL_CMD
-export PATH=~/local/bin:${PATH}                        # ~/local/bin有りの時のサンプル
-# export PATH=~/local/bin:/usr/local/git/bin:${PATH}   # for Mac with git
-export LD_LIBRARY_PATH=~/local/lib:${LD_LIBRARY_PATH}  # ~/local/lib有りのサンプル
+
+# path
+export PATH=~/local/bin:$GOROOT/bin:$GOPATH/bin:$PATH
+export LD_LIBRARY_PATH=~/local/lib64:~/local/lib:$LD_LIBRARY_PATH
 
 ## エイリアス
-# for Mac(GNU系のコマンドはシンボリックリンクを貼り直すことを推奨するので以下コメントアウト)
-#alias ls="gls -F --color"
-#alias find="gfind"
-#alias xargs="gxargs"
 # mysql
 alias my=$MYSQL_CNCT
 alias myq=$MYSQL_CMD
 alias myshow="$MYSQL_CMD 'SHOW TABLES'"
 # ctags
-alias ct="ctags -f .tags --recurse --tag-relative --sort=yes --languages=sh,ruby"
-# silver_searcher
-alias ag="ag --nogroup"
+alias ct='ctags -f .tags --recurse --tag-relative --sort=yes --languages=sh,go,ruby'
 # tig
-alias tigb="tig blame"
-alias tigs="tig status"
-# cd
-alias cdm="~/markdown_texts"
-# apache
-alias ap_err="tailf /var/log/httpd/error_log"
-alias ap_acc="tailf /var/log/httpd/access_log"
+alias tigb='tig blame'
+alias tigs='tig status'
 
 ## グローバルエイリアス
-alias -g TT=">! ~/works/tmp.txt"
+alias -g TT='>! ~/works/tmp.txt'
+
+# for Mac(GNU系のコマンドはシンボリックリンクを貼り直すことを推奨するので以下コメントアウト)
+#alias ls="gls -F --color"
+#alias find="gfind"
+#alias xargs="gxargs"
