5ea623fd186868a37a00701dc22759d378404c5f
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-09-19 18:27:45 +0900

    - gem,zshのインストールスクリプト追加

---
+++ scripts/install-gem-to-cnbundle.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install dir
+CNBUNDLE=~/.cnbundle
+
+bundle instal --path $CNBUNDLE
+
+printf "\ncomplete\n"
+exit 0
---
+++ scripts/install-zsh-to-local.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# base dir
+__FILE__=$_
+BASE_DIR_PATH=${__FILE__%/./*}
+BASE_DIR_PATH=${BASE_DIR_PATH%/scripts}
+CURRENT_DIR=`pwd`
+# zsh src dir
+ZSH_SRC_DIR_PATH=$BASE_DIR_PATH/modules/zsh
+# install dir
+LOCAL_DIR_PATH=~/local
+
+cd $ZSH_SRC_DIR_PATH
+
+./Util/preconfig
+./configure                                     \
+  --prefix=$LOCAL_DIR_PATH                      \
+  --enable-cflags="-I$LOCAL_DIR_PATH/include"   \
+  --enable-cppflags="-I$LOCAL_DIR_PATH/include" \
+  --enable-ldflags="-L$LOCAL_DIR_PATH/lib"      \
+  --enable-multibyte --enable-locale
+make && make install
+
+printf "\ncomplete\n"
+exit 0
