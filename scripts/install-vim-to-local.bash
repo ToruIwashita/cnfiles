78e5e80939044fc839daead4589097532270b01b
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-09-25 18:18:50 +0900

    - scripts/install-vim-to-local.bash追加

---
+++ scripts/install-vim-to-local.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# base dir
+__FILE__=$_
+BASE_DIR_PATH=${__FILE__%/./*}
+BASE_DIR_PATH=${BASE_DIR_PATH%/scripts}
+CURRENT_DIR=`pwd`
+# vim src dir
+VIM_SRC_DIR_PATH=$BASE_DIR_PATH/modules/vim
+# install dir
+LOCAL_DIR_PATH=~/local
+# ruby path
+RUBY_PATH=$1
+
+if [[ -z $RUBY_PATH ]]; then
+  printf "Ruby path is required as argument\n"
+  exit 1
+fi
+
+if [[ $(which luajit) != $LOCAL_DIR_PATH/bin/luajit ]]; then
+  printf "Luajit bin is required in $LOCAL_DIR_PATH\n"
+  exit 1
+fi
+
+cd $VIM_SRC_DIR_PATH
+
+./configure                         \
+  --prefix=$LOCAL_DIR_PATH          \
+  --with-features=huge              \
+  --enable-multibyte                \
+  --enable-xim                      \
+  --enable-fontset                  \
+  --with-ruby-command=$RUBY_PATH    \
+  --enable-rubyinterp=dynamic       \
+  --enable-luainterp=dynamic        \
+  --with-lua-prefix=$LOCAL_DIR_PATH \
+  --with-luajit
+make && make install
+
+git clean -f -d
+
+printf "\ncomplete\n"
+exit 0
