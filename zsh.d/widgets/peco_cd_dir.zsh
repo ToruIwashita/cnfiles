148a45ce86b01a65a047cfe191767219a43a6c70
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-10-14 16:45:57 +0900

    - 不要な変数を削除

--- zsh.d/widgets/peco_cd_dir.zsh
+++ zsh.d/widgets/peco_cd_dir.zsh
@@ -1,12 +1,10 @@
 ## peco-cd-dir
 _peco-cd-dir() {
-  local arg
   local -aU dir_path
 
   [[ "$BUFFER" != '' ]] && print -s "$BUFFER"
 
-  arg=${BUFFER:-*}
-  dir_path=(${(f)"$(find -type d -name "*$arg*" 2>/dev/null | peco 2>/dev/null)"})
+  dir_path=(${(f)"$(find -type d -name "*$BUFFER*" 2>/dev/null | peco 2>/dev/null)"})
   if [[ -z $dir_path ]]; then
     zle beginning-of-line
     return 0
