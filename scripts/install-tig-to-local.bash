c9002e8aa257701e13a91d2883e789bfa0fe1cea
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-10-16 11:10:14 +0900

    - install-tig-to-local.bash追加

---
+++ scripts/install-tig-to-local.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# base dir
+__FILE__=$_
+BASE_DIR_PATH=${__FILE__%/./*}
+BASE_DIR_PATH=${BASE_DIR_PATH%/scripts*}
+CURRENT_DIR=`pwd`
+# tig src dir
+TIG_SRC_DIR_PATH=$BASE_DIR_PATH/modules/tig
+# install dir
+LOCAL_DIR_PATH=~/local
+
+cd $TIG_SRC_DIR_PATH
+
+make
+make install prefix=$LOCAL_DIR_PATH
+
+git checkout .
+git clean -f -d
+
+printf "\ncomplete\n"
+exit 0
