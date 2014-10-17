7c1c39ed9aaabac0a41df114dac1f3f83024f969
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-10-17 10:41:03 +0900

    - gllsの標準出力が見やすくなるように改行を入れる

--- zsh.d/functions/git.zsh
+++ zsh.d/functions/git.zsh
@@ -107,9 +107,9 @@ glls() {
     return 1
   fi
 
-  print "submodules update & pull master"
+  print "submodules update & pull master" && echo
   git submodule update --init
-  git submodule foreach 'git checkout master && git pull origin master'
+  git submodule foreach 'git checkout master && git pull origin master && echo'
 }
 
 gsh() {
