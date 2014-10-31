30fa3a1d16df143cb1581577b5bbf8d2b5163ab6
ToruIwashita <toru.iwashita@pixta.co.jp>
2014-10-31 14:38:04 +0900

    - 変数名変更

--- scripts/install-gems-to-cnbundle.bash
+++ scripts/install-gems-to-cnbundle.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # install dir
-CNBUNDLE=~/.cnbundle
+CNBUNDLE_DIR_PATH=~/.cnbundle
 
-bundle install --path $CNBUNDLE
+bundle install --path $CNBUNDLE_DIR_PATH
 
 printf "\ncomplete\n"
 exit 0
