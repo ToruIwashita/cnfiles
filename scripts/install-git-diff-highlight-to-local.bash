37ed12a13c2169e0818dae5a71565c08532589ce
岩下透 <iwashitatooru@iwashitatooru-no-MacBook-Air.local>
2014-11-14 17:26:01 +0900

    - install-git-diff-highlight-to-local.bash追加

---
+++ scripts/install-git-diff-highlight-to-local.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# base dir
+__FILE__=$_
+BASE_DIR_PATH=$(cd $(dirname $__FILE__);pwd)
+BASE_DIR_PATH=${BASE_DIR_PATH%/scripts*}
+
+# git src dir
+GIT_SRC_DIR_PATH=$BASE_DIR_PATH/modules/git
+# install dir
+LOCAL_BIN_DIR_PATH=~/local/bin
+
+cd $GIT_SRC_DIR_PATH
+
+ln -is $GIT_SRC_DIR_PATH/contrib/diff-highlight/diff-highlight $LOCAL_BIN_DIR_PATH/git-diff-highlight
+
+printf "\ncomplete\n"
+exit 0
