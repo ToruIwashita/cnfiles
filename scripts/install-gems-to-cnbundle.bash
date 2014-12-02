741ad884b8a7b1ba51b046cf1073a3127dc8ca99
toruiwashita <toru.iwashita@crowdworks.jp>
2014-12-02 18:04:45 +0900

    - nodejsとgemsのインストールに更新の処理追加

--- scripts/install-gems-to-cnbundle.bash
+++ scripts/install-gems-to-cnbundle.bash
@@ -8,5 +8,9 @@ gem install pry
 gem install rubocop
 gem install rspec
 
+gem update pry
+gem update rubocop
+gem update rspec
+
 printf "\ncomplete\n"
 exit 0
--- scripts/install-nodejs-packages-to-cnbundle.bash
+++ scripts/install-nodejs-packages-to-cnbundle.bash
@@ -4,6 +4,7 @@
 CNBUNDLE_NODEJS_DIR_PATH=~/.cnbundle/node
 
 npm -g install jshint --prefix=$CNBUNDLE_NODEJS_DIR_PATH
+npm update jshint
 
 printf "\ncomplete\n"
 exit 0
