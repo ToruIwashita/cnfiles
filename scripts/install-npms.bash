ec7f72768ef4c4b8bcd92b580551a3d483970087
toruiwashita <toru.iwashita@crowdworks.jp>
2015-02-06 10:45:13 +0900

    - install-npmsにlice追加

--- scripts/install-npms.bash
+++ scripts/install-npms.bash
@@ -7,9 +7,11 @@ set -e
 
 npm -g install jshint --prefix=$NPM_HOME
 npm -g install coffeelint --prefix=$NPM_HOME
+npm -g install lice --prefix$NPM_HOME
 
 npm update jshint
 npm update coffeelint
+npm update lice
 
 printf "\ncomplete\n"
 exit 0
