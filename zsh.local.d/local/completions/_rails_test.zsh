6eed2b6cf2efd4e1c683318dcaad388bffbb0666
ToruIwashita <toru.iwashita@crowdworks.co.jp>
2015-04-13 19:04:44 +0900

    - rspec用の補完にcurrentという文字列を補完する設定を追加

--- zsh.local.d/local/completions/_rails_test.zsh
+++ zsh.local.d/local/completions/_rails_test.zsh
@@ -1,7 +1,12 @@
 ## rails test関連の補完
+__add-current-of-word() {
+  compadd 'current'
+}
+
 _brspec() {
   _arguments : \
-    '(:)-f[File path]:Input spec file path'
+    '(:)-f[File path]:Input spec file path' \
+    '(:)-t[Tag]:Input spec tag word:__add-current-of-word'
 }
 
 compdef _brspec brspec
