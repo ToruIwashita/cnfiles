904e9212811792345f34b2eba3a4d08bda37da4d
ToruIwashita <toru.iwashita@crowdworks.co.jp>
2015-07-30 12:31:32 +0900

    - install-ctags-to-local.bashをuniversal-ctags(exuberant-ctags)用に変更

--- scripts/install-ctags-to-local.bash
+++ scripts/install-ctags-to-local.bash
@@ -5,10 +5,12 @@ set -e
 
 cd $CTAGS_SRC_DIR_PATH
 
-autoreconf
-./configure --prefix=$LOCAL_DIR_PATH
-make
-make install
+autoreconf -fi
+./configure                \
+  --prefix=$LOCAL_DIR_PATH \
+  --enable-iconv           \
+  --disable-external-sort
+make && make install
 
 git checkout .
 git clean -f -d
