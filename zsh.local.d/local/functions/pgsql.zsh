2ef8e4ef4fe517fc8493a3cabba77759bb71ffe9
ToruIwashita <toru.iwashita@crowdworks.co.jp>
2015-11-05 21:05:24 +0900

    - local/functions/pgsql.zsh追加

---
+++ zsh.local.d/local/functions/pgsql.zsh
@@ -0,0 +1,95 @@
+## PGSQL
+_pg-check-argv() {
+  if (( ! $# )); then
+    print "lack of arguments." 2>&1
+    return 1
+  fi
+
+  if [[ $1:e != "sql" ]]; then
+    print "invalid extension." 2>&1
+    return 1
+  fi
+
+  return 0
+}
+
+pq() {
+  integer arg_num
+  local pg_cmd
+
+  _pg-check-argv $argv
+  if [[ $? -eq 1 ]]; then
+    return 1
+  fi
+
+  pg_cmd="cat $1"
+  for ((i=1; i<$#; i++)); do
+    arg_num=$(expr $i + 1)
+    pg_cmd=$pg_cmd" | sed -e 's/\${$i}/${argv[arg_num]}/g'"
+  done
+
+  print "<<Q"
+  eval $pg_cmd
+  print "Q\n"
+
+  pg_cmd=$pg_cmd" | xargs -0 -i ${PGSQL_CMD} '{}'"
+  print "command: ${pg_cmd}\n"
+  eval $pg_cmd
+}
+
+pqout() {
+  integer arg_num
+  local pg_cmd
+
+  _pg-check-argv $argv
+  if [[ $? -eq 1 ]]; then
+    return 1
+  fi
+
+  pg_cmd="cat $1"
+  for ((i=1; i<$#; i++)); do
+    arg_num=$(expr $i + 1)
+    pg_cmd=$pg_cmd" | sed -e 's/\${$i}/${argv[arg_num]}/g'"
+  done
+
+  print "<<Q"
+  eval $pg_cmd
+  print "Q\n"
+
+  pg_cmd=$pg_cmd" | xargs -0 -i ${PGSQL_CMD} '{}' -N | sed -e 's/\t/,/g' >! ~/works/tmp.txt"
+  print "command: ${pg_cmd}\n"
+  eval $pg_cmd
+}
+
+pqexp() {
+  integer arg_num
+  local pg_cmd
+
+  _pg-check-argv $argv
+  if [[ $? -eq 1 ]]; then
+    return 1
+  fi
+
+  pg_cmd="cat $1"
+  for ((i=1; i<$#; i++)); do
+    arg_num=$(expr $i + 1)
+    pg_cmd=$pg_cmd" | sed -e 's/\${$i}/${argv[arg_num]}/g'"
+  done
+
+  print "<<Q"
+  eval $pg_cmd
+  print "Q\n"
+
+  pg_cmd=$pg_cmd" | xargs -0 -i ${PGSQL_CMD} 'EXPLAIN {}'"
+  print "command: ${pg_cmd}\n"
+  eval $pg_cmd
+}
+
+pgdesc() {
+  if (( ! $# )); then
+    print "lack of arguments." 2>&1
+    return 1
+  fi
+
+  eval "${PGSQL_CMD} '\d ${1}'"
+}
