e72d7194c95c95dba66cb33c430683e883bce08d
ToruIwashita <toru.iwashita@crowdworks.co.jp>
2016-01-20 19:21:10 +0900

    - install-gems.bashにpry-remote追加

--- scripts/install-gems.bash
+++ scripts/install-gems.bash
@@ -8,12 +8,14 @@ printf "GEM_HOME: $GEM_HOME\n"
 
 gem install bundle 
 gem install pry 
+gem install pry-remote 
 gem install rubocop
 gem install rspec
 gem install awesome_print
 
 gem update bundle 
 gem update pry
+gem update pry-remote 
 gem update rubocop
 gem update rspec
 gem update awesome_print
