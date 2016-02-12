8f9bdfabddd2a091290973f8928549b14992aff8
ToruIwashita <toru.iwashita@crowdworks.co.jp>
2016-02-12 17:22:59 +0900

    - immediate_pbcopy.zsh更新

--- zsh.local.d/local/widgets/immediate_pbcopy.zsh
+++ zsh.local.d/local/widgets/immediate_pbcopy.zsh
@@ -4,12 +4,14 @@ _immediate-pbcopy() {
   local -a args
   args=("${(z)BUFFER}")
 
-  if [[ -n ${args[$#args]} ]]; then
-    cmd="cat ${args[$#args]} | pbcopy"
-
-    zle -I
-    print -s $cmd && eval $cmd
+  if (( ! ${#args[$#args]} )); then
+    return 0
   fi
+
+  cmd="cat ${args[$#args]} | pbcopy"
+
+  zle -I
+  print -s $cmd && eval $cmd
 }
 
 zle -N immediate-pbcopy _immediate-pbcopy
