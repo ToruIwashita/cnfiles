3c17aecffee483c6b8e07cd5bdeff8ed4839f9b0
ToruIwashita <toru.iwashita@gmail.com>
2016-04-05 11:36:38 +0900

    - install-gems.bash更新

--- scripts/install-gems.bash
+++ scripts/install-gems.bash
@@ -4,11 +4,13 @@ source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
 ruby -v
+echo
 printf "GEM_HOME: $GEM_HOME\n"
 
 gem install bundler
 gem install rails
 gem install pry 
+gem install pry-byebug
 gem install pry-remote 
 gem install rubocop
 gem install rspec
@@ -17,6 +19,7 @@ gem install awesome_print
 gem update bundler
 gem update rails
 gem update pry
+gem update pry-byebug
 gem update pry-remote 
 gem update rubocop
 gem update rspec
