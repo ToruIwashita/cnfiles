82a7533fcc8da4fa333bc60f05a7ca5a38c94669
ToruIwashita <toru.iwashita@gmail.com>
2016-06-01 11:01:03 +0900

    - install-git-to-local.bash更新

--- scripts/install-git-to-local.bash
+++ scripts/install-git-to-local.bash
@@ -5,6 +5,11 @@ set -e
 
 cd $GIT_SRC_DIR_PATH
 
+# backup
+if [[ -f $LOCAL_BIN_DIR_PATH/git ]]; then
+  mv $LOCAL_BIN_DIR_PATH/{git,git.prev}
+fi
+
 make configure
 ./configure --prefix=$LOCAL_DIR_PATH
 make && make install
