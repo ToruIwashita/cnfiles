1e00b3a78bcc581b04bf714b832ffe52767fed30
ToruIwashita <toru.iwashita@gmail.com>
2016-07-12 11:35:53 +0900

    - scripts/install-gems.bashをrbenv execを利用するように変更

--- scripts/install-gems.bash
+++ scripts/install-gems.bash
@@ -7,23 +7,26 @@ ruby -v
 echo
 printf "GEM_HOME: $GEM_HOME\n"
 
-gem install bundler
-gem install rails
-gem install pry 
-gem install pry-byebug
-gem install pry-remote 
-gem install rubocop
-gem install rspec
-gem install awesome_print
+rbenv exec gem install awesome_print
+rbenv exec gem install bundler
+rbenv exec gem install rails
+rbenv exec gem install pry
+rbenv exec gem install pry-byebug
+rbenv exec gem install pry-remote
+rbenv exec gem install rubocop
+rbenv exec gem install rspec
+rbenv exec gem install awesome_print
 
-gem update bundler
-gem update rails
-gem update pry
-gem update pry-byebug
-gem update pry-remote 
-gem update rubocop
-gem update rspec
-gem update awesome_print
+rbenv exec gem update bundler
+rbenv exec gem update rails
+rbenv exec gem update pry
+rbenv exec gem update pry-byebug
+rbenv exec gem update pry-remote
+rbenv exec gem update rubocop
+rbenv exec gem update rspec
+rbenv exec gem update awesome_print
+
+rbenv rehash
 
 printf "\ncomplete\n"
 exit 0
