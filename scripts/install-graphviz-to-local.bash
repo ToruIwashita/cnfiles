b453b041ab80d1c057ccb610139988bad19ea38f
ToruIwashita <toru.iwashita@gmail.com>
2016-08-23 15:08:11 +0900

    - scripts/install-graphviz-to-local.bash追加

---
+++ scripts/install-graphviz-to-local.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source $(cd $(dirname $_);pwd)/env.bash
+set -e
+
+cd $GRAPHVIZ_SRC_DIR_PATH
+
+./autogen.sh
+./configure --prefix=$LOCAL_DIR_PATH
+make && make install
+
+git checkout .
+git clean -f -d
+
+printf "\ncomplete\n"
+exit 0
