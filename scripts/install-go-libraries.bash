21682c3ac7b883ac349c2e358c93d6ad4cbc1f5d
ToruIwashita <toru.iwashita@gmail.com>
2016-08-24 11:56:22 +0900

    - scripts/install-go-libraries.bashにgo-pdebugを追加

--- scripts/install-go-libraries.bash
+++ scripts/install-go-libraries.bash
@@ -8,6 +8,7 @@ go get github.com/google/btree
 go get github.com/jessevdk/go-flags
 go get github.com/mattn/go-runewidth
 go get github.com/nsf/termbox-go
+go get github.com/lestrrat/go-pdebug
 
 go get -u all
 
