312de8fe84394ab1aa20342235c12184383cacab
ToruIwashita <toru.iwashita@gmail.com>
2016-09-06 18:24:21 +0900

    - scripts/install-glide-to-local.bash更新(とりあえずcurlでインストール)

--- scripts/install-glide-to-local.bash
+++ scripts/install-glide-to-local.bash
@@ -3,25 +3,7 @@
 source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
-cd $GLIDE_SRC_DIR_PATH
-
-# github.com/Mastrminds dir
-if [[ -d $GLIDE_GITHUB_DIR_PATH_IN_GOPATH ]]; then
-  printf "\n$GLIDE_GITHUB_DIR_PATH_IN_GOPATH dir already exists\n"
-else
-  printf "\nmkdir $GLIDE_GITHUB_DIR_PATH_IN_GOPATH\n"
-  mkdir -p $GLIDE_GITHUB_DIR_PATH_IN_GOPATH
-fi
-
-# create glide dir symlink
-if [[ -L $GLIDE_GITHUB_DIR_PATH_IN_GOPATH/glide ]]; then
-  printf "\n$GLIDE_GITHUB_DIR_PATH_IN_GOPATH/glide symlink already exists\n"
-else
-  printf "\ncreate symlink $GLIDE_GITHUB_DIR_PATH_IN_GOPATH/glide\n"
-  ln -is $GLIDE_SRC_DIR_PATH $GLIDE_GITHUB_DIR_PATH_IN_GOPATH/glide
-fi
-
-make build
+curl https://glide.sh/get | sh
 
 printf "\ncomplete\n"
 exit 0
