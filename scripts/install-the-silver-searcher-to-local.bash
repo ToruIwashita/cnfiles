893b5bfe1d3327966f3bb90ba0a16005ce4031a5
ToruIwashita <toru.iwashita@gmail.com>
2016-11-28 13:02:47 +0900

    - scripts/install-the-silver-searcher-to-local.bash更新

--- scripts/install-the-silver-searcher-to-local.bash
+++ scripts/install-the-silver-searcher-to-local.bash
@@ -5,6 +5,11 @@ set -e
 
 cd $THE_SILVER_SEARCHER_SRC_DIR_PATH
 
+# backup
+if [[ -f $LOCAL_BIN_DIR_PATH/ag  ]]; then
+  mv $LOCAL_BIN_DIR_PATH/{ag,ag.prev}
+fi
+
 ./build.sh --prefix=$LOCAL_DIR_PATH
 make install PREFIX=$LOCAL_DIR_PATH
 
