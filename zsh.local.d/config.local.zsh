9af2ef42c9af2def437717e36b7bdfb9508aec40
ToruIwashita <toru.iwashita@gmail.com>
2017-04-19 16:53:15 +0900

    - zsh.local.d/config.local.zsh更新

--- zsh.local.d/config.local.zsh
+++ zsh.local.d/config.local.zsh
@@ -14,6 +14,12 @@ alias cdsg='cd ~/src/go'
 alias cdw='cd ~/works'
 alias cdc='cd ~/.cnfiles'
 alias cdml='cd ~/.memolist'
+# rbenv
+alias bundle='rbenv exec bundle'
+alias pry='rbenv exec pry'
+alias rails='rbenv exec rails'
+alias rspec='rbenv exec rspec'
+alias aws='rbenv exec aws'
 # ctags
 alias ct='ctags -f .tags --recurse --tag-relative --sort=yes --languages=sh,go,ruby'
 # rails
