c5b682f305691358abb3ada112499f5a32a605ba
ToruIwashita <toru.iwashita@gmail.com>
2017-06-06 15:51:25 +0900

    - scripts/install-npms.bash更新

--- scripts/install-npms.bash
+++ scripts/install-npms.bash
@@ -3,6 +3,7 @@
 source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
+# install for global
 npm -g install jshint --prefix=$NPM_HOME
 npm -g install coffeelint --prefix=$NPM_HOME
 npm -g install lice --prefix$NPM_HOME
