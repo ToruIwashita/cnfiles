ef367272ea360cc643b311b45e03219be7e6609e
ToruIwashita <toru.iwashita@gmail.com>
2017-06-15 14:36:27 +0900

    - scripts/install-python-env-to-local.bash更新

--- scripts/install-python-env-to-local.bash
+++ scripts/install-python-env-to-local.bash
@@ -3,12 +3,12 @@
 source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
-cd $PYENV_SRC_DIR_PATH
-ls bin/ | while read line; do
-  if [[ ! -L $LOCAL_BIN_DIR_PATH/$line ]]; then
-    ln -is $PYENV_SRC_DIR_PATH/bin/$line $LOCAL_BIN_DIR_PATH/$line
-  fi
-done
+if [[ -L ~/.pyenv ]]; then
+  printf "\e[32m~/.pyenv dir symlink already exists\e[0m\n"
+else
+  printf "\e[31mcreate symlink ~/.pyenv dir\e[0m\n"
+  ln -isn $PYENV_SRC_DIR_PATH ~/.pyenv
+fi
 
-printf "complete\n"
+printf "\ncomplete\n"
 exit 0
