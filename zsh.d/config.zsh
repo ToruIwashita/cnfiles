74e6b2ee464de392ce4f18fac3f8f39346e6db3a
ToruIwashita <toru.iwashita@gmail.com>
2017-07-25 14:25:02 +0900

    - zsh.d/config.zsh更新

--- zsh.d/config.zsh
+++ zsh.d/config.zsh
@@ -135,7 +135,7 @@ alias gsp='git stash pop'
 # ping
 alias ping-one='ping -v -c 1'
 # other
-alias ez='exec zsh -l'
+alias ez="exec $SHELL -l"
 alias cp='cp -ip'
 alias mv='mv -i'
 alias rm='rm -i'
