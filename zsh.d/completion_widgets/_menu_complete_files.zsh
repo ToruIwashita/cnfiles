59b06a9195b605d1eb6ddca4a7a02f49f50ea0b7
ToruIwashita <toru.iwashita@gmail.com>
2017-07-28 12:42:20 +0900

    - zsh.d/completion_widgets/_menu_complete_files.zsh更新

--- zsh.d/completion_widgets/_menu_complete_files.zsh
+++ zsh.d/completion_widgets/_menu_complete_files.zsh
@@ -3,3 +3,5 @@
 # zstyle ':completion:*'の設定をmenu-complete-filesに反映させるため一度_genericに設定する
 zle -C menu-complete-files menu-complete _generic
 zstyle ':completion:menu-complete-files:*' completer _files
+zstyle ':completion:menu-complete-files:*' menu select interactive
+zstyle ':completion:menu-complete-files:*' list-colors ${(s.:.)LS_COLORS}
