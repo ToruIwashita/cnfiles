d59c4b3ec14c11db790ca698207816d26fdddab5
ToruIwashita <toru.iwashita@gmail.com>
2017-09-21 11:41:31 +0900

    - scripts/install-gems.bash更新

--- scripts/install-gems.bash
+++ scripts/install-gems.bash
@@ -15,6 +15,7 @@ echo
 rbenv exec gem install bundler
 rbenv exec gem install aws-sdk
 rbenv exec gem install rails
+rbenv exec gem install rspec
 rbenv exec gem install pry
 rbenv exec gem install pry-doc
 rbenv exec gem install pry-byebug
@@ -22,7 +23,7 @@ rbenv exec gem install pry-remote
 rbenv exec gem install pry-stack_explorer
 rbenv exec gem install pry-rails
 rbenv exec gem install rubocop
-rbenv exec gem install rspec
+rbenv exec gem install rubocop-rspec
 rbenv exec gem install awesome_print
 
 echo
@@ -32,6 +33,7 @@ echo
 rbenv exec gem update bundler
 rbenv exec gem update aws-sdk
 rbenv exec gem update rails
+rbenv exec gem update rspec
 rbenv exec gem update pry
 rbenv exec gem update pry-doc
 rbenv exec gem update pry-byebug
@@ -39,7 +41,7 @@ rbenv exec gem update pry-remote
 rbenv exec gem update pry-stack_explorer
 rbenv exec gem update pry-rails
 rbenv exec gem update rubocop
-rbenv exec gem update rspec
+rbenv exec gem install rubocop-rspec
 rbenv exec gem update awesome_print
 
 echo
