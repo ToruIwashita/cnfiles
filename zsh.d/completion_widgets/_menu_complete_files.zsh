231e60de9035ff2bcac93c9ba051e75adddb544f
ToruIwashita <toru.iwashita@gmail.com>
2017-10-26 13:25:48 +0900

    - _menu_complete_files.zsh削除

--- zsh.d/completion_widgets/_menu_complete_files.zsh
+++
@@ -1,7 +0,0 @@
-# _menu-compete-files
-
-# zstyle ':completion:*'の設定をmenu-complete-filesに反映させるため一度_genericに設定する
-zle -C menu-complete-files menu-complete _generic
-zstyle ':completion:menu-complete-files:*' completer _files
-zstyle ':completion:menu-complete-files:*' menu select interactive
-zstyle ':completion:menu-complete-files:*' list-colors ${(s.:.)LS_COLORS}
