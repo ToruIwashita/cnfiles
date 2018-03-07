0a596119463fb77731ecd4652608652135d28038
ToruIwashita <toru.iwashita@gmail.com>
2018-03-07 18:36:00 +0900

    - curl.zsh更新

--- zsh.d/completions/_curl.zsh
+++ zsh.d/completions/_curl.zsh
@@ -1,7 +1,10 @@
 ## curl関数用補完
 _cl() {
   _arguments \
+    '(-c --cookie)'{-c,--cookie}'[Cookie]' \
     '(-j --json)'{-j,--json}'["Accept: application/json" and "Content-Type: application/json" header]' \
+    '(-p --params)'{-p,--params}'[Parameter]' \
+    '--post[Post request]' \
     '(-v --verbose)'{-f,--fixup}'[verbose mode]' \
     '(-h --help)'{-h,--help}'[Show help text]'
 }
--- zsh.d/functions/curl.zsh
+++ zsh.d/functions/curl.zsh
@@ -6,8 +6,12 @@ cl() {
   self_cmd=$0
   help="Try \`$self_cmd --help' for more information."
   usage=`cat <<EOF
-usage: $self_cmd <URL> [-j --json]
+usage: $self_cmd <URL> [-c --cookie <key=value>]
+                [-j --json]
+                [-p --params <key=value>]
+                [--post]
                 [-v --verbose]
+                [-h --help]
 EOF`
 
   # default options
@@ -15,8 +19,29 @@ EOF`
 
   while (( $# > 0 )); do
     case "$1" in
+      -c | --cookie)
+        if (( ! $#2  )) || [[ "$2" =~ ^-+  ]]; then
+          print "$self_cmd: option requires an argument -- '$1'\n$help" 1>&2
+          return 1
+        fi
+        options+=("-b '$2'")
+        shift 2
+        ;;
       -j | --json)
         options+=('-H "Accept: application/json" -H "Content-Type: application/json"')
+        shift 1
+        ;;
+      -p | --params)
+        if (( ! $#2  )) || [[ "$2" =~ ^-+  ]]; then
+          print "$self_cmd: option requires an argument -- '$1'\n$help" 1>&2
+          return 1
+        fi
+        options+=("-d '$2'")
+        shift 2
+        ;;
+      --post)
+        options=(${options:#-I})
+        options+=('-X POST')
         shift 1
         ;;
       -v | --verbose)
