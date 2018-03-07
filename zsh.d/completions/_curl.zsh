3ad70eb5c79d8190db67e8baca153f3dace3be58
ToruIwashita <toru.iwashita@gmail.com>
2018-03-07 18:38:24 +0900

    - _curl.zsh更新

--- zsh.d/completions/_curl.zsh
+++ zsh.d/completions/_curl.zsh
@@ -1,11 +1,11 @@
 ## curl関数用補完
 _cl() {
   _arguments \
-    '(-c --cookie)'{-c,--cookie}'[Cookie]' \
-    '(-j --json)'{-j,--json}'["Accept: application/json" and "Content-Type: application/json" header]' \
-    '(-p --params)'{-p,--params}'[Parameter]' \
+    '(-c --cookie)'{-c,--cookie}'[Set cookie]' \
+    '(-j --json)'{-j,--json}'[With "Accept: application/json" and "Content-Type: application/json" header]' \
+    '(-p --params)'{-p,--params}'[Set parameter]' \
     '--post[Post request]' \
-    '(-v --verbose)'{-f,--fixup}'[verbose mode]' \
+    '(-v --verbose)'{-f,--fixup}'[Verbose mode]' \
     '(-h --help)'{-h,--help}'[Show help text]'
 }
 
