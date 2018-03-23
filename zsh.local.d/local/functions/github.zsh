e47823a21ffb1bee472f99370e8798fc87b8a456
ToruIwashita <toru.iwashita@gmail.com>
2018-03-23 10:58:10 +0900

    - local/functions/github.zsh追加

---
+++ zsh.local.d/local/functions/github.zsh
@@ -0,0 +1,13 @@
+github-traffic() {
+  local repo
+
+  for repo in $OWNED_GITHUB_REPOSITORIES; do
+    print $repo
+    print "stars: $(curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/stargazers 2> /dev/null | jq -c 'length')"
+    print 'views:'
+    curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/views 2>/dev/null| jq -c '.views[]'
+    print 'clones:'
+    curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/clones 2>/dev/null | jq -c '.clones[]'
+    echo
+  done
+}
