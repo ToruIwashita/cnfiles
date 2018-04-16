6fd14c4e64cb150d27334c0de62d212f950c8c3d
ToruIwashita <toru.iwashita@gmail.com>
2018-04-16 18:39:00 +0900

    - github.zsh更新

--- zsh.local.d/local/functions/github.zsh
+++ zsh.local.d/local/functions/github.zsh
@@ -2,10 +2,9 @@ github-traffic() {
   local repo
 
   for repo in $OWNED_GITHUB_REPOSITORIES; do
-    print $repo
-    print "stars: $(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/stargazers 2> /dev/null | jq -c 'length')"
+    curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/users/$GITHUB_USER_NAME/repos 2>/dev/null | jq 'map(select(.name == "'$repo'")) | map({name, stargazers_count, open_issues_count}) | .[]'
     print 'views:'
-    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/views 2>/dev/null| jq -c '.views[]'
+    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/views 2>/dev/null | jq -c '.views[]'
     print 'clones:'
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/clones 2>/dev/null | jq -c '.clones[]'
     echo
