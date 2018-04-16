983e5d8b1e412a2074e6b7f997f7b9b587c7c7f1
ToruIwashita <toru.iwashita@gmail.com>
2018-04-16 20:23:51 +0900

    - github.zsh更新

--- zsh.local.d/local/functions/github.zsh
+++ zsh.local.d/local/functions/github.zsh
@@ -2,7 +2,7 @@ github-traffic() {
   local repo
 
   for repo in $OWNED_GITHUB_REPOSITORIES; do
-    curl -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/users/$GITHUB_USER_NAME/repos 2>/dev/null | jq 'map(select(.name == "'$repo'")) | map({name, stargazers_count, open_issues_count}) | .[]'
+    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/users/$GITHUB_USER_NAME/repos 2>/dev/null | jq 'map(select(.name == "'$repo'")) | map({name, stargazers_count, open_issues_count}) | .[]'
     print 'views:'
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/views 2>/dev/null | jq -c '.views[]'
     print 'clones:'
