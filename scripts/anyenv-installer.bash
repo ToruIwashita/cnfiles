0bf08a89fe09fc4ec0452c13b230e67d403b9153
ToruIwashita <toru.iwashita@gmail.com>
2018-04-20 13:56:47 +0900

    - scripts/anyenv-installer.bash更新

--- scripts/anyenv-installer.bash
+++ scripts/anyenv-installer.bash
@@ -7,23 +7,23 @@ ANYENV_ROOT=~/.anyenv
 
 export PATH="$PATH:$ANYENV_ROOT/bin"
 
-RUBY_VERSION=2.5.0
-PYTHON3_VERSION=3.6.4
-PYTHON2_VERSION=2.7.14
-LUA_VERSION='luajit-2.0.5'
-GO_VERSION=1.10.0
-NODE_VERSION=9.8.0
-SCALA_VERSION='scala-2.12.4'
-SBT_VERSION='sbt-0.13.9'
+DEFAULT_RUBY_VERSION=2.5.0
+DEFAULT_PYTHON3_VERSION=3.6.4
+DEFAULT_PYTHON2_VERSION=2.7.14
+DEFAULT_LUA_VERSION='luajit-2.0.5'
+DEFAULT_GO_VERSION=1.10.0
+DEFAULT_NODE_VERSION=9.8.0
+DEFAULT_SCALA_VERSION='scala-2.12.4'
+DEFAULT_SBT_VERSION='sbt-0.13.9'
 
-USE_RUBY_VERSION=${USE_RUBY_VERSION:-$RUBY_VERSION}
-USE_PYTHON3_VERSION=${USE_PYTHON3_VERSION:-$PYTHON3_VERSION}
-USE_PYTHON2_VERSION=${USE_PYTHON2_VERSION:-$PYTHON2_VERSION}
-USE_LUA_VERSION=${USE_LUA_VERSION:-$LUA_VERSION}
-USE_GO_VERSION=${USE_GO_VERSION:-$GO_VERSION}
-USE_NODE_VERSION=${USE_NODE_VERSION:-$NODE_VERSION}
-USE_SCALA_VERSION=${USE_SCALA_VERSION:-$SCALA_VERSION}
-USE_SBT_VERSION=${USE_SBT_VERSION:-$SBT_VERSION}
+USE_RUBY_VERSION=${USE_RUBY_VERSION:-$DEFAULT_RUBY_VERSION}
+USE_PYTHON3_VERSION=${USE_PYTHON3_VERSION:-$DEFAULT_PYTHON3_VERSION}
+USE_PYTHON2_VERSION=${USE_PYTHON2_VERSION:-$DEFAULT_PYTHON2_VERSION}
+USE_LUA_VERSION=${USE_LUA_VERSION:-$DEFAULT_LUA_VERSION}
+USE_GO_VERSION=${USE_GO_VERSION:-$DEFAULT_GO_VERSION}
+USE_NODE_VERSION=${USE_NODE_VERSION:-$DEFAULT_NODE_VERSION}
+USE_SCALA_VERSION=${USE_SCALA_VERSION:-$DEFAULT_SCALA_VERSION}
+USE_SBT_VERSION=${USE_SBT_VERSION:-$DEFAULT_SBT_VERSION}
 
 eval "$(anyenv init -)"
 
@@ -62,53 +62,53 @@ if [[ ! $(which sbtenv) ]]; then
   anyenv install sbtenv
 fi
 
-if [[ ($USE_RUBY_VERSION != $RUBY_VERSION && ! $(rbenv versions | grep "[- ]$USE_RUBY_VERSION")) || ! $(which ruby) =~ anyenv ]]; then
+if [[ ! $(which ruby) =~ anyenv || ! $(rbenv versions | grep "[- ]$USE_RUBY_VERSION") ]]; then
   CONFIGURE_OPTS='--enable-shared' rbenv install $USE_RUBY_VERSION
 fi
 
 rbenv global $USE_RUBY_VERSION
 rbenv rehash
 
-if [[ ($USE_PYTHON3_VERSION != $PYTHON3_VERSION && ! $(pyenv versions | grep "[- ]$USE_PYTHON3_VERSION")) || ! $(which python3) =~ anyenv ]]; then
+if [[ ! $(which python3) =~ anyenv || ! $(pyenv versions | grep "[- ]$USE_PYTHON3_VERSION") ]]; then
   CONFIGURE_OPTS='--enable-shared' pyenv install $USE_PYTHON3_VERSION
 fi
 
-if [[ ($USE_PYTHON2_VERSION != $PYTHON2_VERSION && ! $(pyenv versions | grep "[- ]$USE_PYTHON2_VERSION")) || ! $(which python2) =~ anyenv ]]; then
+if [[ ! $(which python2) =~ anyenv || ! $(pyenv versions | grep "[- ]$USE_PYTHON2_VERSION") ]]; then
   CONFIGURE_OPTS='--enable-shared' pyenv install $USE_PYTHON2_VERSION
 fi
 
 pyenv global $USE_PYTHON3_VERSION $USE_PYTHON2_VERSION
 pyenv rehash
 
-if [[ ($USE_LUA_VERSION != $LUA_VERSION && ! $(luaenv versions | grep "[- ]$USE_LUA_VERSION")) || ! $(which lua) =~ anyenv ]]; then
+if [[ ! $(which lua) =~ anyenv || ! $(luaenv versions | grep "[- ]$USE_LUA_VERSION") ]]; then
   CONFIGURE_OPTS='--enable-shared' luaenv install $USE_LUA_VERSION
 fi
 
 luaenv global $USE_LUA_VERSION
 luaenv rehash
 
-if [[ ($USE_GO_VERSION != $GO_VERSION && ! $(goenv versions | grep "[- ]$USE_GO_VERSION")) || ! $(which go) =~ anyenv ]]; then
+if [[ ! $(which go) =~ anyenv || ! $(goenv versions | grep "[- ]$USE_GO_VERSION") ]]; then
   CONFIGURE_OPTS='--enable-shared' goenv install $USE_GO_VERSION
 fi
 
 goenv global $USE_GO_VERSION
 goenv rehash
 
-if [[ ($USE_NODE_VERSION != $NODE_VERSION && ! $(nodenv versions | grep "[- ]$USE_NODE_VERSION")) || ! $(which node) =~ anyenv ]]; then
+if [[ ! $(which node) =~ anyenv || ! $(nodenv versions | grep "[- ]$USE_NODE_VERSION") ]]; then
   nodenv install $USE_NODE_VERSION
 fi
 
 nodenv global $USE_NODE_VERSION
 nodenv rehash
 
-if [[ ($USE_SCALA_VERSION != $SCALA_VERSION && ! $(scalaenv versions | grep "[- ]$USE_SCALA_VERSION")) || ! $(which scala) =~ anyenv ]]; then
+if [[ ! $(which scala) =~ anyenv || ! $(scalaenv versions | grep "[- ]$USE_SCALA_VERSION") ]]; then
   scalaenv install $USE_SCALA_VERSION
 fi
 
 scalaenv global $USE_SCALA_VERSION
 scalaenv rehash
 
-if [[ ($USE_SBT_VERSION != $SBT_VERSION && ! $(sbtenv versions | grep "[- ]$USE_SBT_VERSION")) || ! $(which sbt) =~ anyenv ]]; then
+if [[ ! $(which sbt) =~ anyenv || ! $(sbtenv versions | grep "[- ]$USE_SBT_VERSION") ]]; then
   sbtenv install $USE_SBT_VERSION
 fi
 
