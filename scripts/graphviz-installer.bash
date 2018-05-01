76b693038d72aff98287abc6f947da220c11ca05
ToruIwashita <toru.iwashita@gmail.com>
2018-05-01 18:55:55 +0900

    - scripts/graphviz-installer.bash更新

--- scripts/graphviz-installer.bash
+++ scripts/graphviz-installer.bash
@@ -3,10 +3,26 @@
 source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
+# Xquartz(X11)
+XQUARTZ_PATH=$(which Xquartz)
+XQUARTZ_INCLUDE_PATH="${XQUARTZ_PATH%/bin*}/include"
+
+if [[ $XQUARTZ_PATH == '' ]]; then
+  printf "there is no Xquartz installed\n"
+  exit 1
+fi
+
 cd $GRAPHVIZ_SRC_DIR_PATH
 
+# backup
+if [[ -f $LOCAL_BIN_DIR_PATH/dot ]]; then
+  mv $LOCAL_BIN_DIR_PATH/{dot,dot.prev}
+fi
+
 ./autogen.sh
-./configure --prefix=$LOCAL_DIR_PATH
+./configure \
+  --prefix=$LOCAL_DIR_PATH \
+  CPPFLAGS=-I$XQUARTZ_INCLUDE_PATH
 make && make install
 
 git checkout .
