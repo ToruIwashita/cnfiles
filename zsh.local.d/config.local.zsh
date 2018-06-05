221f21d55ce0a9f9eb75c39c9c63c8f852cbbff6
ToruIwashita <toru.iwashita@gmail.com>
2018-06-05 21:00:53 +0900

    - zsh.local.d/config.local.zsh更新

--- zsh.local.d/config.local.zsh
+++ zsh.local.d/config.local.zsh
@@ -6,7 +6,9 @@ alias sbt-c='sbt ~console'
 alias sbt-p='sbt ~compile'
 alias sbt-r='sbt ~run'
 # python
-alias pip-r='pipenv run python'
+alias pip-r='pipenv run'
+# django
+alias pjango-m='pipenv run python manage.py'
 # rails
 alias brails-s='RAILS_ENV=development bundle exec rails s -b 0.0.0.0 --port 3000'
 alias brails-c='RAILS_ENV=development bundle exec rails c'
