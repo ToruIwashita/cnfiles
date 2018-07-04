2f4ca37d4d42d75ca8ccf45f94069ad6439e3fdd
ToruIwashita <toru.iwashita@gmail.com>
2018-07-04 16:50:06 +0900

    - scripts/npms-installer.bash更新

--- scripts/npms-installer.bash
+++ scripts/npms-installer.bash
@@ -12,11 +12,11 @@ printf "npm: $(npm --version)\n"
 
 # install for global
 nodenv exec npm -g install vue-cli
-nodenv exec npm -g install jshint
 nodenv exec npm -g install eslint
 nodenv exec npm -g install eslint-plugin-import
 nodenv exec npm -g install eslint-plugin-vue
 nodenv exec npm -g install eslint-config-vue
+nodenv exec npm -g install babel-eslint
 nodenv exec npm -g install stylelint
 nodenv exec npm -g install stylelint-config-standard
 nodenv exec npm -g install stylelint-processor-html
@@ -27,10 +27,10 @@ nodenv exec npm update eslint
 nodenv exec npm update eslint-plugin-import
 nodenv exec npm update eslint-plugin-vue
 nodenv exec npm update eslint-config-vue
+nodenv exec npm update babel-eslint
 nodenv exec npm update stylelint
 nodenv exec npm update stylelint-config-standard
 nodenv exec npm update stylelint-processor-html
-nodenv exec npm update jshint
 nodenv exec npm update lice
 
 printf "\ncomplete\n"
