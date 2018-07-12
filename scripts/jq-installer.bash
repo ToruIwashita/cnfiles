fb5fafcd4c7639a739a13f7f34d97fdaec40c5d0
ToruIwashita <toru.iwashita@gmail.com>
2018-07-12 10:34:13 +0900

    - scripts/jq-installer.bash更新

--- scripts/jq-installer.bash
+++ scripts/jq-installer.bash
@@ -10,15 +10,20 @@ if [[ -f $LOCAL_BIN_DIR_PATH/jq ]]; then
   mv $LOCAL_BIN_DIR_PATH/{jq,jq.prev}
 fi
 
-autoreconf -i
+git submodule update --init
+autoreconf -fi
 ./configure                 \
   --prefix=$LOCAL_DIR_PATH  \
+  --with-oniguruma=builtin  \
   --disable-maintainer-mode
 make -j8
 make install
 
 git checkout .
 git clean -f -d
+
+cd modules/oniguruma
+git checkout .
 
 printf "\ncomplete\n"
 exit 0
