286428759780e379815a294248059b3def08eac5
ToruIwashita <toru.iwashita@gmail.com>
2019-01-31 15:58:24 +0900

    - kube.zshにkube-pod-login()を追加

--- zsh.local.d/local/functions/kube.zsh
+++ zsh.local.d/local/functions/kube.zsh
@@ -1,3 +1,4 @@
+# kube
 watch-kube-cn() {
   watch -c "echo '# context'; kubectx; echo; echo '# namespace'; kubens"
 }
@@ -14,4 +15,28 @@ show-k8s-tool-versions() {
 
   print '# helm'
   helm version
+}
+
+kube-pod-login() {
+  local answer
+  local pod_name container_name
+
+  while :; do
+    print "context: $(kubectl config current-context)"
+
+    read answer
+    case "$answer" in
+      *)
+        break
+        ;;
+    esac
+  done
+
+  pod_name=(${(f)"$(kubectl get pods --server-print=false | peco --select-1 2>/dev/null | cut -f 1 -d ' ')"})
+  container_name=${pod_name%-*-*}
+
+  print "login container: $container_name"
+  echo
+
+  kubectl exec -it $pod_name --container $container_name sh
 }
