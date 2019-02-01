a5689501de06819d541a6bc8858db9905bf0e63d
ToruIwashita <toru.iwashita@gmail.com>
2019-02-01 18:58:50 +0900

    - docker.zsh更新

--- zsh.local.d/local/functions/docker.zsh
+++ zsh.local.d/local/functions/docker.zsh
@@ -5,7 +5,7 @@ watch-docker-ps() {
 docker-container-login() {
   local container_id container_name peco_resulting_line
 
-  peco_resulting_line=(${(f)"$(docker container ls | tail -n +2 | peco --select-1 2>/dev/null)"})
+  peco_resulting_line=(${(f)"$(docker container ls | tail -n +2 | awk '{ print $1 "  " $NF }' | peco --select-1 2>/dev/null)"})
 
   if (( ! $#peco_resulting_line )); then
     return
@@ -14,8 +14,6 @@ docker-container-login() {
   container_id=${peco_resulting_line%% *}
   container_name=${peco_resulting_line##* }
 
-  print "Login container: $container_name"
-  echo
-
+  print "docker exec -it $container_id sh"
   docker exec -it $container_id sh
 }
