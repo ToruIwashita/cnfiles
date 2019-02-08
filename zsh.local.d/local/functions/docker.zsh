0d0813442405b178f0b716c0292b52b5fecc2c55
ToruIwashita <toru.iwashita@gmail.com>
2019-02-08 14:56:24 +0900

    - docker.zsh更新

--- zsh.local.d/local/functions/docker.zsh
+++ zsh.local.d/local/functions/docker.zsh
@@ -3,7 +3,7 @@ watch-docker-ps() {
 }
 
 docker-container-login() {
-  local container_id container_name peco_resulting_line
+  local container_id peco_resulting_line
 
   peco_resulting_line=(${(f)"$(docker container ls | tail -n +2 | awk '{ print $1 "  " $NF }' | peco --select-1 2>/dev/null)"})
 
@@ -12,8 +12,26 @@ docker-container-login() {
   fi
 
   container_id=${peco_resulting_line%% *}
-  container_name=${peco_resulting_line##* }
+
+  echo
 
   print "docker exec -it $container_id sh"
   docker exec -it $container_id sh
+}
+
+docker-container-attach() {
+  local container_id peco_resulting_line
+
+  peco_resulting_line=(${(f)"$(docker container ls | tail -n +2 | awk '{ print $1 "  " $NF }' | peco --select-1 2>/dev/null)"})
+
+  if (( ! $#peco_resulting_line )); then
+    return
+  fi
+
+  container_id=${peco_resulting_line%% *}
+
+  echo
+
+  print "docker attach $container_id"
+  docker attach $container_id
 }
