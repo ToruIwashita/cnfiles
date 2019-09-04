288cc5d5b95ad1731131c4e53465c468d46f128b
ToruIwashita <toru.iwashita@gmail.com>
2019-09-04 11:25:27 +0900

    - zsh.local.d/local/functions/github.zsh更新

--- zsh.local.d/local/functions/github.zsh
+++ zsh.local.d/local/functions/github.zsh
@@ -2,9 +2,8 @@ github-traffic() {
   local repo
 
   for repo in $OWNED_GITHUB_REPOSITORIES; do
-    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/users/$GITHUB_USER_NAME/repos 2>/dev/null | jq 'map(select(.name == "'$repo'")) | map({name, stargazers_count, open_issues_count}) | .[]'
-    print 'subscribers:'
-    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo 2> /dev/null | jq -c '.subscribers_count'
+    curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/users/$GITHUB_USER_NAME/repos 2>/dev/null | jq 'map(select(.name == "'$repo'")) | map({name, stargazers_count, forks_count, open_issues_count}) | .[]'
+    print "subscribers: $(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo 2> /dev/null | jq -c '.subscribers_count')"
     print 'views:'
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_USER_NAME/$repo/traffic/views 2>/dev/null | jq -c '.views[]'
     print 'clones:'
