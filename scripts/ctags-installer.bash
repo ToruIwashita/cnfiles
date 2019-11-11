a0482da8f0844786727135aee81807d253363635
ToruIwashita <toru.iwashita@gmail.com>
2019-11-11 15:55:14 +0900

    - scripts/ctags-installer.bash更新

--- scripts/ctags-installer.bash
+++ scripts/ctags-installer.bash
@@ -10,11 +10,17 @@ if [[ -f $LOCAL_BIN_DIR_PATH/ctags ]]; then
   mv $LOCAL_BIN_DIR_PATH/{ctags,ctags.prev}
 fi
 
+LIBXML2_PATH=/usr/local/opt/libxml2
+
 ./autogen.sh
-./configure                \
-  --prefix=$LOCAL_DIR_PATH \
-  --enable-iconv           \
-  --disable-external-sort
+./configure                                     \
+  --prefix=$LOCAL_DIR_PATH                      \
+  --enable-iconv                                \
+  --disable-external-sort                       \
+  PKG_CONFIG_PATH="$LIBXML2_PATH/lib/pkgconfig" \
+  LDFLAGS="-L$LIBXML2_PATH/lib"                 \
+  CPPFLAGS="-I$LIBXML2_PATH/include"
+
 make && make install
 
 git checkout .
