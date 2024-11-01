2fc763057a0f62d37ca7cf27a001aaf00d70f379
ToruIwashita <toru.iwashita@gmail.com>
2024-11-01 13:55:41 +0900

    - scripts/pypies-installer.bash更新

--- scripts/pypies-installer.bash
+++ scripts/pypies-installer.bash
@@ -19,8 +19,6 @@ pyenv exec pip3 install --upgrade pip
 echo
 pyenv exec pip3 install -U pipenv
 pyenv exec pip3 install -U django
-pyenv exec pip3 install -U awscli
-pyenv exec pip3 install -U aws-mfa
 pyenv exec pip3 install -U pytest
 pyenv exec pip3 install -U vim-vint
 pyenv exec pip3 install -U sqlformat
