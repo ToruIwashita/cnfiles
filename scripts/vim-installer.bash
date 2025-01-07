d859a3912db024d7d65855a55376d5f03d11e22d
ToruIwashita <toru.iwashita@gmail.com>
2025-01-07 14:40:40 +0900

    - scripts/vim-installer.bash更新

--- scripts/vim-installer.bash
+++ scripts/vim-installer.bash
@@ -3,42 +3,42 @@
 source $(cd $(dirname $_);pwd)/env.bash
 set -e
 
-# anyenv
-ANYENV_DIR_PATH=~/.anyenv
-ANYENV_ENVS_DIR_PATH=$ANYENV_DIR_PATH/envs
+# asdf
+ASDF_DIR_PATH=~/.asdf
+ASDF_ENVS_DIR_PATH=$ASDF_DIR_PATH/installs
 
 # ruby
 RUBY_PATH=${1:-$(which ruby)}
 RUBY_VERSION_STDOUT=$(ruby -v 2>&1)
 PREFIX_REMOVED_RUBY_VERSION_STDOUT=${RUBY_VERSION_STDOUT#*[[:space:]]}
 RUBY_VERSION=${PREFIX_REMOVED_RUBY_VERSION_STDOUT%%p*}
-RUBY_LIB_DIR=$ANYENV_ENVS_DIR_PATH/rbenv/versions/$RUBY_VERSION/lib
+RUBY_LIB_DIR=$ASDF_ENVS_DIR_PATH/ruby/$RUBY_VERSION/lib
 
 # python
 PYTHON2_PATH=${1:-$(which python2)}
 PYTHON2_VERSION_STDOUT=$(python2 --version 2>&1)
 PYTHON2_VERSION=${PYTHON2_VERSION_STDOUT#*[[:space:]]}
-PYTHON2_LIB_DIR=$ANYENV_ENVS_DIR_PATH/pyenv/versions/$PYTHON2_VERSION/lib
+PYTHON2_LIB_DIR=$ASDF_ENVS_DIR_PATH/python/$PYTHON2_VERSION/lib
 PYTHON3_PATH=${1:-$(which python3)}
 PYTHON3_VERSION_STDOUT=$(python3 --version 2>&1)
 PYTHON3_VERSION=${PYTHON3_VERSION_STDOUT#*[[:space:]]}
-PYTHON3_LIB_DIR=$ANYENV_ENVS_DIR_PATH/pyenv/versions/$PYTHON3_VERSION/lib
+PYTHON3_LIB_DIR=$ASDF_ENVS_DIR_PATH/python/$PYTHON3_VERSION/lib
 
 # check ruby
-if [[ ! $RUBY_PATH =~ anyenv ]]; then
-  printf "there is no ruby installed with anyenv\n"
+if [[ ! $RUBY_PATH =~ asdf ]]; then
+  printf "there is no ruby installed with asdf\n"
   exit 1
 fi
 
 # check python2
-if [[ ! $PYTHON2_PATH =~ anyenv ]]; then
-  printf "there is no python2 installed with anyenv\n"
+if [[ ! $PYTHON2_PATH =~ asdf ]]; then
+  printf "there is no python2 installed with asdf\n"
   exit 1
 fi
 
 # check python3
-if [[ ! $PYTHON3_PATH =~ anyenv ]]; then
-  printf "there is no python3 installed with anyenv\n"
+if [[ ! $PYTHON3_PATH =~ asdf ]]; then
+  printf "there is no python3 installed with asdf\n"
   exit 1
 fi
 
