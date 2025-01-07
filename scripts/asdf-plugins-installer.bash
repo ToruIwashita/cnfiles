075aee8a87a43b540b496c0d5186eee4dbd69f82
ToruIwashita <toru.iwashita@gmail.com>
2025-01-07 10:31:18 +0900

    - scripts/asdf-plugins-installer.bash追加

---
+++ scripts/asdf-plugins-installer.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+source $(cd $(dirname $_);pwd)/env.bash
+set -e
+
+DEFAULT_RUBY_VERSION=3.1.5
+DEFAULT_PYTHON2_VERSION=2.7.18
+DEFAULT_PYTHON3_VERSION=3.12.7
+DEFAULT_GO_VERSION=1.23.4
+DEFAULT_NODE_VERSION=18.20.3
+
+USE_RUBY_VERSION=${USE_RUBY_VERSION:-$DEFAULT_RUBY_VERSION}
+USE_PYTHON2_VERSION=${USE_PYTHON2_VERSION:-$DEFAULT_PYTHON2_VERSION}
+USE_PYTHON3_VERSION=${USE_PYTHON3_VERSION:-$DEFAULT_PYTHON3_VERSION}
+USE_GO_VERSION=${USE_GO_VERSION:-$DEFAULT_GO_VERSION}
+USE_NODE_VERSION=${USE_NODE_VERSION:-$DEFAULT_NODE_VERSION}
+
+printf "install plugins\n"
+echo
+
+asdf plugin add ruby
+asdf plugin add python
+asdf plugin add golang
+asdf plugin add nodejs
+
+printf "\nbefore versions:\n"
+echo
+
+asdf list
+
+echo
+
+asdf install ruby $USE_RUBY_VERSION
+asdf global ruby $USE_RUBY_VERSION
+
+asdf install python $USE_PYTHON2_VERSION
+asdf install python $USE_PYTHON3_VERSION
+asdf global python $USE_PYTHON3_VERSION $USE_PYTHON2_VERSION
+
+asdf install golang $USE_GO_VERSION
+asdf global golang $USE_GO_VERSION
+
+asdf install nodejs $USE_NODE_VERSION
+asdf global nodejs $USE_NODE_VERSION
+
+printf "\nafter versions:\n"
+echo
+
+asdf list
+
+printf "\ncomplete\n"
+
+exit 0
