a4ed10d4c033f8e63d704cb5e6e60b0d25bb120e
ToruIwashita <toru.iwashita@gmail.com>
2025-02-25 21:18:38 +0900

    - python2削除

--- scripts/vim-installer.bash
+++ scripts/vim-installer.bash
@@ -11,14 +11,10 @@ ASDF_ENVS_DIR_PATH=$ASDF_DIR_PATH/installs
 RUBY_PATH=${1:-$(which ruby)}
 RUBY_VERSION_STDOUT=$(ruby -v 2>&1)
 PREFIX_REMOVED_RUBY_VERSION_STDOUT=${RUBY_VERSION_STDOUT#*[[:space:]]}
-RUBY_VERSION=${PREFIX_REMOVED_RUBY_VERSION_STDOUT%%p*}
+RUBY_VERSION=${PREFIX_REMOVED_RUBY_VERSION_STDOUT%%[[:space:]]*}
 RUBY_LIB_DIR=$ASDF_ENVS_DIR_PATH/ruby/$RUBY_VERSION/lib
 
 # python
-PYTHON2_PATH=${1:-$(which python2)}
-PYTHON2_VERSION_STDOUT=$(python2 --version 2>&1)
-PYTHON2_VERSION=${PYTHON2_VERSION_STDOUT#*[[:space:]]}
-PYTHON2_LIB_DIR=$ASDF_ENVS_DIR_PATH/python/$PYTHON2_VERSION/lib
 PYTHON3_PATH=${1:-$(which python3)}
 PYTHON3_VERSION_STDOUT=$(python3 --version 2>&1)
 PYTHON3_VERSION=${PYTHON3_VERSION_STDOUT#*[[:space:]]}
@@ -27,12 +23,6 @@ PYTHON3_LIB_DIR=$ASDF_ENVS_DIR_PATH/python/$PYTHON3_VERSION/lib
 # check ruby
 if [[ ! $RUBY_PATH =~ asdf ]]; then
   printf "there is no ruby installed with asdf\n"
-  exit 1
-fi
-
-# check python2
-if [[ ! $PYTHON2_PATH =~ asdf ]]; then
-  printf "there is no python2 installed with asdf\n"
   exit 1
 fi
 
@@ -60,10 +50,9 @@ fi
   --enable-xim                   \
   --enable-fontset               \
   --enable-rubyinterp=dynamic    \
-  --enable-pythoninterp=dynamic  \
   --enable-python3interp=dynamic \
   --enable-fail-if-missing       \
-  LDFLAGS=-Wl,-rpath,$RUBY_LIB_DIR,-rpath,$PYTHON3_LIB_DIR,-rpath,$PYTHON2_LIB_DIR
+  LDFLAGS=-Wl,-rpath,$RUBY_LIB_DIR,-rpath,$PYTHON3_LIB_DIR
 
 make && make install
 
