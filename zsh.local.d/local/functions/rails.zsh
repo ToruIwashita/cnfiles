94d08249a9e4695f1689c1adccb089501211e9f9
ToruIwashita <toru.iwashita@gmail.com>
2025-04-24 11:20:44 +0900

    - zsh.local.d/local/functions/rails.zsh更新

--- zsh.local.d/local/functions/rails.zsh
+++ zsh.local.d/local/functions/rails.zsh
@@ -3,3 +3,7 @@ brails-clear-cache() {
   bundle exec rails assets:clean
   bundle exec rails tmp:cache:clear
 }
+
+tailf-main-started-log() {
+  tail -f $* | grep --line-buffered -v "OPTIONS" | grep --line-buffered "INFO -- : Started"
+}
