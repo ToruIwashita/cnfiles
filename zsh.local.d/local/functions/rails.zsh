6c90b5fbe3a8b0e6006de3f7e50e2424c2ed8b0f
ToruIwashita <toru.iwashita@gmail.com>
2025-05-02 15:35:43 +0900

    - zsh.local.d/local/functions/rails.zsh更新

--- zsh.local.d/local/functions/rails.zsh
+++ zsh.local.d/local/functions/rails.zsh
@@ -5,5 +5,10 @@ brails-clear-cache() {
 }
 
 tailf-main-started-log() {
+  if (( ! $# )); then
+    print 'lack of arguments.' 2>&1
+    return 1
+  fi
+
   tail -f $* | grep --line-buffered -v 'OPTIONS' | grep --line-buffered 'INFO -- : Started'
 }
