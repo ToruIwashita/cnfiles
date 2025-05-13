340c705b9276074097e093dcdf3cc6f3e0bc8e1e
ToruIwashita <toru.iwashita@gmail.com>
2025-05-13 18:11:43 +0900

    - zsh.local.d/local/completions/_rspec.zsh更新

--- zsh.local.d/local/completions/_rspec.zsh
+++ zsh.local.d/local/completions/_rspec.zsh
@@ -19,11 +19,11 @@ __git-untracked-spec-files() {
 _brspec() {
   _arguments \
     '(-c --changed-file)'{-c,--changed-file}'[With changed file completion]: :__git-changed-spec-files' \
-    '(-e --example)'{-e,--example}'[Filter test by string]' \
-    '(-f --fail-fast)'{-f,--fail-fast}'[Fail fast]' \
+    '(-e --example)'{-e,--example}'[Filter test by string]: :_nothing' \
+    '(-f --fail-fast)'{-f,--fail-fast}'[Fail fast]: :_nothing' \
     '(-m --modified-file)'{-m,--modified-file}'[With modified file completion]: :__git-modified-spec-files' \
     '(-n --no-binstub)'{-n,--no-binstub}'[Do not use binstub]' \
-    '(-s --seed)'{-s,--seed}'[Specify the seed value]' \
+    '(-s --seed)'{-s,--seed}'[Specify the seed value]: :_nothing' \
     '(-t --tag)'{-t,--tag}'[Tag name]: :__word-of-current' \
     '(-u --untracked-file)'{-u,--untracked-file}'[With untraced file completion]: :__git-untracked-spec-files' \
     '(-h --help)'{-h,--help}'[Show help text]' \
