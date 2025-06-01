8c5fbe3274119cc7946d1a7d1b95669d98a79188
ToruIwashita <toru.iwashita@gmail.com>
2025-06-01 22:33:41 +0900

    - zsh.d/widgets/peco_ag_and_start_editor.zsh,zsh.d/widgets/peco_git_grep_and_start_editor.zsh修正

--- zsh.d/widgets/peco_ag_and_start_editor.zsh
+++ zsh.d/widgets/peco_ag_and_start_editor.zsh
@@ -1,7 +1,7 @@
 ## peco-ag-and-start-editor
 _peco-ag-and-start-editor() {
   local context parttern specified_line
-  local -a args peco_resulting_line
+  local -a args peco_resulting_lines
   local -aU file_paths
 
   (( $#BUFFER )) && print -s "$BUFFER"
@@ -16,19 +16,19 @@ _peco-ag-and-start-editor() {
     pattern=$args
   fi
 
-  peco_resulting_line=(${(f)"$(ag --silent $context "$pattern" | peco --select-1 2>/dev/null)"})
+  peco_resulting_lines=(${(f)"$(ag --silent $context "$pattern" | peco --select-1 2>/dev/null)"})
 
-  if (( ! $#peco_resulting_line )); then
+  if (( ! $#peco_resulting_lines )); then
     zle beginning-of-line
     return
   fi
 
-  if (( $#peco_resulting_line == 1 )); then
-    specified_line=+${${(M)${(M)peco_resulting_line#*[:][0-9]*[:-]}%[:][0-9]*[:-]}//[:-]/}
-    file_paths=${(R)${(M)peco_resulting_line#*[:][0-9]*[:-]}%[:][0-9]*[:-]}
+  if (( $#peco_resulting_lines == 1 )); then
+    specified_line=+${${(M)${(M)peco_resulting_lines#*[:][0-9]*[:-]}%[:][0-9]*[:-]}//[:-]/}
+    file_paths=${(R)${(M)peco_resulting_lines#*[:][0-9]*[:-]}%[:][0-9]*[:-]}
   else
     specified_line=''
-    file_paths=${(R)${(M)peco_resulting_line#*[:][0-9]*[:-]}%[:][0-9]*[:-]}
+    file_paths=${(R)${(M)peco_resulting_lines#*[:][0-9]*[:-]}%[:][0-9]*[:-]}
   fi
 
   BUFFER="${EDITOR:-vim} $file_paths $specified_line"
--- zsh.d/widgets/peco_git_grep_and_start_editor.zsh
+++ zsh.d/widgets/peco_git_grep_and_start_editor.zsh
@@ -1,7 +1,7 @@
 ## peco-git-grep-and-start-editor
 _peco-git-grep-and-start-editor() {
   local specified_line
-  local -a peco_resulting_line
+  local -a peco_resulting_lines
   local -aU file_paths
 
   (( $#BUFFER )) && print -s "$BUFFER"
@@ -11,19 +11,19 @@ _peco-git-grep-and-start-editor() {
     return
   fi
 
-  peco_resulting_line=(${(f)"$(git grep --line-number "$BUFFER" | peco --select-1 2>/dev/null)"})
+  peco_resulting_lines=(${(f)"$(git grep --line-number "$BUFFER" | peco --select-1 2>/dev/null)"})
 
-  if (( ! $#peco_resulting_line )); then
+  if (( ! $#peco_resulting_lines )); then
     zle beginning-of-line
     return
   fi
 
-  if (( $#peco_resulting_line == 1 )); then
-    specified_line=+${${(M)${(M)peco_resulting_line#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}//[:-]/}
-    file_paths=${(R)${(M)peco_resulting_line#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}
+  if (( $#peco_resulting_lines == 1 )); then
+    specified_line=+${${(M)${(M)peco_resulting_lines#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}//[:-]/}
+    file_paths=${(R)${(M)peco_resulting_lines#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}
   else
     specified_line=''
-    file_paths=${(R)${(M)peco_resulting_line#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}
+    file_paths=${(R)${(M)peco_resulting_lines#*[:-][0-9]*[:-]}%[:-][0-9]*[:-]}
   fi
 
   BUFFER="${EDITOR:-vim} $file_paths $specified_line"
