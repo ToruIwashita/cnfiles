40cbcd9df477541eb7938f8cb06bfe08abf4d64b
ToruIwashita <toru.iwashita@gmail.com>
2025-06-05 09:04:13 +0900

    - scripts/npms-installer.bash更新

--- scripts/npms-installer.bash
+++ scripts/npms-installer.bash
@@ -27,6 +27,7 @@ npm install -g lice
 npm install -g @redocly/cli
 npm instlal -g swagger-merger
 npm install -g @mermaid-js/mermaid-cli
+npm install -g @anthropic-ai/claude-code
 
 asdf reshim nodejs
 
