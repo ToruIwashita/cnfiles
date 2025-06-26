dcff94f6943ed313b45f5e226b529b9660d5132a
ToruIwashita <toru.iwashita@gmail.com>
2025-06-27 08:38:45 +0900

    - scripts/npms-installer.bash更新

--- scripts/npms-installer.bash
+++ scripts/npms-installer.bash
@@ -17,40 +17,51 @@ echo
 npm update -g
 
 echo
-printf 'npm install -g eslint'
+printf 'npm install -g eslint\n'
 npm install -g eslint
 
-printf 'npm install -g prettier'
+echo
+printf 'npm install -g prettier\n'
 npm install -g prettier
 
-printf 'npm install -g babel-eslint'
+echo
+printf 'npm install -g babel-eslint\n'
 npm install -g babel-eslint
 
-printf 'npm install -g eslint-plugin-prettier'
+echo
+printf 'npm install -g eslint-plugin-prettier\n'
 npm install -g eslint-plugin-prettier
 
-printf 'npm install -g eslint-config-prettier'
+echo
+printf 'npm install -g eslint-config-prettier\n'
 npm install -g eslint-config-prettier
 
-printf 'npm install -g fixjson'
+echo
+printf 'npm install -g fixjson\n'
 npm install -g fixjson
 
-printf 'npm install -g lice'
+echo
+printf 'npm install -g lice\n'
 npm install -g lice
 
-printf 'npm install -g @redocly/cli'
+echo
+printf 'npm install -g @redocly/cli\n'
 npm install -g @redocly/cli
 
-printf 'npm instlal -g swagger-merger'
-npm instlal -g swagger-merger
+echo
+printf 'npm install -g swagger-merger\n'
+npm install -g swagger-merger
 
-printf 'npm install -g @mermaid-js/mermaid-cli'
+echo
+printf 'npm install -g @mermaid-js/mermaid-cli\n'
 npm install -g @mermaid-js/mermaid-cli
 
-printf 'npm install -g @anthropic-ai/claude-code'
+echo
+printf 'npm install -g @anthropic-ai/claude-code\n'
 npm install -g @anthropic-ai/claude-code
 
-printf 'npm install -g @google/gemini-cli'
+echo
+printf 'npm install -g @google/gemini-cli\n'
 npm install -g @google/gemini-cli
 
 asdf reshim nodejs
