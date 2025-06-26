09f96baaa09c85d71b2a7e019bb5a06b073ed913
ToruIwashita <toru.iwashita@gmail.com>
2025-06-26 23:17:20 +0900

    - scripts/npms-installer.bash更新

--- scripts/npms-installer.bash
+++ scripts/npms-installer.bash
@@ -17,17 +17,41 @@ echo
 npm update -g
 
 echo
+printf 'npm install -g eslint'
 npm install -g eslint
+
+printf 'npm install -g prettier'
 npm install -g prettier
+
+printf 'npm install -g babel-eslint'
 npm install -g babel-eslint
+
+printf 'npm install -g eslint-plugin-prettier'
 npm install -g eslint-plugin-prettier
+
+printf 'npm install -g eslint-config-prettier'
 npm install -g eslint-config-prettier
+
+printf 'npm install -g fixjson'
 npm install -g fixjson
+
+printf 'npm install -g lice'
 npm install -g lice
+
+printf 'npm install -g @redocly/cli'
 npm install -g @redocly/cli
+
+printf 'npm instlal -g swagger-merger'
 npm instlal -g swagger-merger
+
+printf 'npm install -g @mermaid-js/mermaid-cli'
 npm install -g @mermaid-js/mermaid-cli
+
+printf 'npm install -g @anthropic-ai/claude-code'
 npm install -g @anthropic-ai/claude-code
+
+printf 'npm install -g @google/gemini-cli'
+npm install -g @google/gemini-cli
 
 asdf reshim nodejs
 
