8fb1d0d7242273773499a3ab77fa38a26cb545bf
ToruIwashita <toru.iwashita@gmail.com>
2025-08-07 14:11:05 +0900

    - zsh.local.d/local/widgets/immediate_path_pbcopy.zsh更新

--- zsh.local.d/local/widgets/immediate_path_pbcopy.zsh
+++ zsh.local.d/local/widgets/immediate_path_pbcopy.zsh
@@ -1,23 +1,29 @@
 ## immediate-path-pbcopy
 _immediate-path-pbcopy() {
-  local cmd full_path target_path
+  local cmd unquoted_arg_path raw_full_path real_full_path quoted_real_full_path
   local -a args
 
   if (( $#BUFFER )); then
     args=("${(z)BUFFER}")
 
-    if [[ "${args[$#args]}" = /* ]]; then
-      full_path="${(A)args[$#args]}"
+    # ファイル存在チェック用にエスケープを解除
+    unquoted_arg_path="${(Q)args[$#args]}"
+
+    if [[ "${unquoted_arg_path}" = /* ]]; then
+      real_full_path="${unquoted_arg_path:A}"
     else
-      full_path="${(A)PWD}/${args[$#args]}"
+      raw_full_path="${PWD}/${unquoted_arg_path}"
+      real_full_path="${raw_full_path:A}"
     fi
 
-    if [[ ! -e "$full_path" ]]; then
+    if [[ ! -e "$real_full_path" ]]; then
       zle beginning-of-line
       return
     fi
 
-    cmd="echo -n ${(q)${full_path/#$HOME/~}} | pbcopy"
+    # フルパスをエスケープ付きでクリップボードにコピー
+    quoted_real_full_path="${(q)real_full_path}"
+    cmd="echo -n ${(q)${quoted_real_full_path/#$HOME/~}} | pbcopy"
   else
     cmd="echo -n ${(q)${PWD/#$HOME/~}} | pbcopy"
   fi
