0b85fc2ec599a22c1450b0eb3399b9d6a2dde5ae
ToruIwashita <toru.iwashita@gmail.com>
2026-03-13 15:22:33 +0900

    Update scripts/init.bash

--- scripts/init.bash
+++ scripts/init.bash
@@ -151,12 +151,12 @@ else
   cp -p $BASE_DIR_PATH/my.cnf $BASE_DIR_PATH/my.local.cnf
 fi
 
-## copy github-markdown-css/github-markdown-dark.css file
-if [[ -f $CONFIG_DIR_PATH/github-markdown-dark.css ]]; then
-  printf "\e[32m$CONFIG_DIR_PATH/github-markdown-dark.css file already exists\e[0m\n"
+## copy highlight.js/src/styles/github-dark.css file
+if [[ -f $CONFIG_DIR_PATH/github-dark.css ]]; then
+  printf "\e[32m$CONFIG_DIR_PATH/github-dark.css file already exists\e[0m\n"
 else
-  printf "\e[31mcopy file $BASE_DIR_PATH/modules/github-markdown-css/github-markdown-dark.css to $CONFIG_DIR_PATH/github-markdown-dark.css\e[0m\n"
-  cp -p $BASE_DIR_PATH/modules/github-markdown-css/github-markdown-dark.css $CONFIG_DIR_PATH/github-markdown-dark.css
+  printf "\e[31mcopy file $BASE_DIR_PATH/modules/highlight.js/src/styles/github-dark.css to $CONFIG_DIR_PATH/github-dark.css\e[0m\n"
+  cp -p $BASE_DIR_PATH/modules/highlight.js/src/styles/github-dark.css $CONFIG_DIR_PATH/github-dark.css
 fi
 
 # zsh
@@ -543,10 +543,6 @@ else
   printf "\e[31mcreate symlink ~/.codex/prompts dir\e[0m\n"
   ln -is $MARKDOWN_TEXTS_DIR_PATH/agentic_coding/commands ~/.codex/prompts
 fi
-
-# text replacement
-sed -e 's/\.markdown-body/#body/g' $CONFIG_DIR_PATH/github-markdown-dark.css > $CONFIG_DIR_PATH/github-markdown-dark.css.tmp
-mv -f $CONFIG_DIR_PATH/github-markdown-dark.css.tmp $CONFIG_DIR_PATH/github-markdown-dark.css
 
 printf "\ninit complete\n"
 exit 0
