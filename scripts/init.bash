2368414d45064c03074fd2877e1aa84c5e189a89
ToruIwashita <toru.iwashita@gmail.com>
2026-07-16 11:04:29 +0900

    Update scripts/init.bash

--- scripts/init.bash
+++ scripts/init.bash
@@ -498,6 +498,14 @@ else
   ln -is $BASE_DIR_PATH/local/bin/copy-tmp-dump-html-to-tmp-html $LOCAL_BIN_DIR_PATH/copy-tmp-dump-html-to-tmp-html
 fi
 
+# claude_code_statusline
+if [[ -L $LOCAL_BIN_DIR_PATH/claude_code_statusline ]]; then
+  _printf "\e[32m$LOCAL_BIN_DIR_PATH/claude_code_statusline symlink already exists\e[0m\n"
+else
+  _printf "\e[31mcreate symlink $LOCAL_BIN_DIR_PATH/claude_code_statusline\e[0m\n"
+  ln -is $BASE_DIR_PATH/local/bin/claude_code_statusline $LOCAL_BIN_DIR_PATH/claude_code_statusline
+fi
+
 # open (macOS open)
 if [[ -L $LOCAL_BIN_DIR_PATH/open ]]; then
   _printf "\e[32m$LOCAL_BIN_DIR_PATH/open symlink already exists\e[0m\n"
