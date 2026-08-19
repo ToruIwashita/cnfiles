febcb7130665dd6be6192932050c977c8ea3eb56
ToruIwashita <toru.iwashita@gmail.com>
2026-08-19 16:06:59 +0900

    Update zsh.d/lib/ai_agent_session.zsh

--- zsh.d/lib/ai_agent_session.zsh
+++ zsh.d/lib/ai_agent_session.zsh
@@ -20,6 +20,28 @@ __ai-agent-session-add() {
   print "${session_id} - ${session_name}@${session_dir}" >> $sessions_file
 }
 
+__ai-agent-session-update-id() {
+  local session_name session_id sessions_file
+
+  session_name=$1
+  session_id=$2
+  sessions_file=$AI_AGENT_SESSIONS_FILE_PATH
+
+  if [[ ! -f $sessions_file ]]; then
+    return 1
+  fi
+
+  if (( $(grep -Fc " - ${session_name}@" $sessions_file) != 1 )); then
+    return 1
+  fi
+
+  awk -v name=" - ${session_name}@" -v id="$session_id" '
+    index($0, name) { sub(/^[^ ]* - /, id " - ") }
+    { print }
+  ' $sessions_file > ${sessions_file}.tmp
+  mv ${sessions_file}.tmp $sessions_file
+}
+
 __ai-agent-session-list() {
   local sessions_file
 
