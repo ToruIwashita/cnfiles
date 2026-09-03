4a58a36b052cc900d5938c4d27f552d3a42e0883
ToruIwashita <toru.iwashita@gmail.com>
2026-09-04 01:45:41 +0900

    Update zsh.local.d/local/widgets/immediate_path_pbcopy.zsh

--- zsh.local.d/local/widgets/immediate_path_pbcopy.zsh
+++ zsh.local.d/local/widgets/immediate_path_pbcopy.zsh
@@ -1,6 +1,6 @@
 ## immediate-path-pbcopy
 _immediate-path-pbcopy() {
-  local cmd unquoted_arg_path raw_full_path real_full_path quoted_real_full_path
+  local cmd unquoted_arg_path expanded_arg_path raw_full_path real_full_path quoted_real_full_path
   local -a args
 
   if (( $#BUFFER )); then
@@ -8,11 +8,13 @@ _immediate-path-pbcopy() {
 
     # ファイル存在チェック用にエスケープを解除
     unquoted_arg_path="${(Q)args[$#args]}"
+    # BUFFER中の ~ は (z) でも (Q) でも展開されず,そのままだと $PWD 配下として解決され存在チェックに失敗する
+    expanded_arg_path="${unquoted_arg_path/#\~\//$HOME/}"
 
-    if [[ "${unquoted_arg_path}" = /* ]]; then
-      real_full_path="${unquoted_arg_path:A}"
+    if [[ "${expanded_arg_path}" = /* ]]; then
+      real_full_path="${expanded_arg_path:A}"
     else
-      raw_full_path="${PWD}/${unquoted_arg_path}"
+      raw_full_path="${PWD}/${expanded_arg_path}"
       real_full_path="${raw_full_path:A}"
     fi
 
